3766a9a287f7b6a920a876de2c7732cbff8001a1
federico.cristina <118191354+fcristina77@users.noreply.github.com>
2012-06-29 17:41:12 +0000

    Replicación.  Correcciones menores a los procesos Origen y Destino, así como al acceso a los mismos desde terminal.

--- base/src/org/openXpertya/replication/ReplicationBuilder.java
+++ base/src/org/openXpertya/replication/ReplicationBuilder.java
@@ -89,94 +89,102 @@ public class ReplicationBuilder extends ChangeLogXMLBuilder {
 			m_replicationXMLData = new StringBuffer("");
 			m_replicationXMLData.append("<changegroup");
 			
-			// Creo, seteo el valor de los nodos atributos y asocio con el nodo padre
-			m_replicationXMLData.append(" tableName=\"").append(group.getTableName()).append("\"");
-			m_replicationXMLData.append(" uid=\"").append(group.getAd_componentObjectUID()).append("\"");  // <- el uid es usado para el recordUID (retrieveUID)
-			m_replicationXMLData.append(" operation=\"").append(group.getOperation()).append("\"");
-			m_replicationXMLData.append(">");
-			
-			// Itero por los elementos del grupo
-			for (ChangeLogElement element : group.getElements()) {
-				// Creo el nodo elemento para la columna
-				m_replicationXMLData.append("<column");
-				m_replicationXMLData.append(" name=\"").append(element.getColumnName()).append("\"");
-				m_replicationXMLData.append(" type=\"").append(element.getAD_Reference_ID()).append("\"");
-				
-				// Si es de tipo referencia a una tabla o de tipo Entero con _ID, 
-				// coloco los atributos específicos
-				isTableReference = isTableReference(element);
-				useRetrieveUID = false;
-				retrieveUIDValue = "";
-				if(isTableReference && element.getNewValue() != null && element.getNewValue().toString().length() > 0 
-						&& !getIgnoresReferenceColumns().contains(element.getColumnName())){
-					// Determinar el nombre de la tabla de referencia
-					tableName = element.getColumnName();
-					if(element.getColumnName().toUpperCase().endsWith("_ID")){
-						tableName = element.getColumnName().substring(0,element.getColumnName().lastIndexOf("_"));
-					}
-					// Si tiene una referencia seteada en la columna, entonces busco ahí la tabla
-					if(element.getAD_Reference_Value_ID() != 0){
-						tableID = ReplicationCache.referencesData.get(element.getAD_Reference_Value_ID()); 
-						tableName = ReplicationCache.tablesData.get(tableID);  
-					}
-					
-					/**
-					 *  Si la referencia posee un retrieveUID, entonces se deberá utilizar éste en lugar del ID local
-					 */
-					// La tabla referenciada tiene campo retrieveUID?
-					tableHasRetrieveUID = ReplicationCache.tablesWithRetrieveUID.contains(tableName.toLowerCase()); 
-					if (tableHasRetrieveUID)
-					{
-						// El registro tiene seteado el retrieveUID? Si no lo tiene, es una refTable tradicional
-						retrieveUIDValue = DB.getSQLValueString(trxName, " SELECT retrieveUID FROM " + tableName + " WHERE " + tableName + "_ID = " + element.getNewValue() + " AND 1 = ?", 1 );
-						if (retrieveUIDValue != null && retrieveUIDValue.length() > 0)
-							useRetrieveUID = true;
-					}
-					// incorporar la referencia a la tabla correspondiente
-					m_replicationXMLData.append(" refTable=\"").append(tableName).append("\"");
-				}
-				
-				// cierre de tag inicial de la columna
+			// En caso de error al crear el xml para un changegroup, se omite el mismo, pero se continua con los siguientes
+			try
+			{
+				// Creo, seteo el valor de los nodos atributos y asocio con el nodo padre
+				m_replicationXMLData.append(" tableName=\"").append(group.getTableName()).append("\"");
+				m_replicationXMLData.append(" uid=\"").append(group.getAd_componentObjectUID()).append("\"");  // <- el uid es usado para el recordUID (retrieveUID)
+				m_replicationXMLData.append(" operation=\"").append(group.getOperation()).append("\"");
 				m_replicationXMLData.append(">");
 				
-				// Textos del nodo, old y new values
-			    newValue = useRetrieveUID ? (UID_REFERENCE_PREFIX+retrieveUIDValue) : String.valueOf(element.getNewValue());
-//				if(element.getBinaryValue() != null){
-//					/** TODO: VER QUE HACER ACA CON LOS BINARIOS EN REPLICACIÓN! */
-//				}
-				// En el AD_Org_ID en realidad no se envia el AD_Org_ID sino el host asociado (replicationArrayPos) cargado, 
-				// dado que este es el único valor en común.  Para AD_Org_ID = 0, pasamos directamente ese valor sin mapear 
-				if ("AD_Org_ID".equalsIgnoreCase(element.getColumnName()) && !"UID=AD_Org-0".equals(newValue))
-				{
-					// Si useRetrieveUID es false, entonces UID no estará seteado.  Sin embargo, AD_Org_ID podrìa ser 0 en este caso
-					// el cual tambien es necesario omitir, dejando también 0 como valor.
-					if (!"0".equals(newValue)) 
-					{
-						Integer orgPos = ReplicationCache.map_RepArrayPos_OrgID.get(newValue.replace("UID=AD_Org-", ""));
-						if (orgPos == null)
-							throw new Exception ("No hay mapeo posible para la organización " + newValue + " en la tabla de hosts de replicación ");
-						newValue = orgPos.toString();
+				// Itero por los elementos del grupo
+				for (ChangeLogElement element : group.getElements()) {
+					// Creo el nodo elemento para la columna
+					m_replicationXMLData.append("<column");
+					m_replicationXMLData.append(" name=\"").append(element.getColumnName()).append("\"");
+					m_replicationXMLData.append(" type=\"").append(element.getAD_Reference_ID()).append("\"");
+					
+					// Si es de tipo referencia a una tabla o de tipo Entero con _ID, 
+					// coloco los atributos específicos
+					isTableReference = isTableReference(element);
+					useRetrieveUID = false;
+					retrieveUIDValue = "";
+					if(isTableReference && element.getNewValue() != null && element.getNewValue().toString().length() > 0 
+							&& !getIgnoresReferenceColumns().contains(element.getColumnName())){
+						// Determinar el nombre de la tabla de referencia
+						tableName = element.getColumnName();
+						if(element.getColumnName().toUpperCase().endsWith("_ID")){
+							tableName = element.getColumnName().substring(0,element.getColumnName().lastIndexOf("_"));
+						}
+						// Si tiene una referencia seteada en la columna, entonces busco ahí la tabla
+						if(element.getAD_Reference_Value_ID() != 0){
+							tableID = ReplicationCache.referencesData.get(element.getAD_Reference_Value_ID()); 
+							tableName = ReplicationCache.tablesData.get(tableID);  
+						}
+						
+						/**
+						 *  Si la referencia posee un retrieveUID, entonces se deberá utilizar éste en lugar del ID local
+						 */
+						// La tabla referenciada tiene campo retrieveUID?
+						tableHasRetrieveUID = ReplicationCache.tablesWithRetrieveUID.contains(tableName.toLowerCase()); 
+						if (tableHasRetrieveUID)
+						{
+							// El registro tiene seteado el retrieveUID? Si no lo tiene, es una refTable tradicional
+							retrieveUIDValue = DB.getSQLValueString(trxName, " SELECT retrieveUID FROM " + tableName + " WHERE " + tableName + "_ID = " + element.getNewValue() + " AND 1 = ?", 1 );
+							if (retrieveUIDValue != null && retrieveUIDValue.length() > 0)
+								useRetrieveUID = true;
+						}
+						// incorporar la referencia a la tabla correspondiente
+						m_replicationXMLData.append(" refTable=\"").append(tableName).append("\"");
 					}
+					
+					// cierre de tag inicial de la columna
+					m_replicationXMLData.append(">");
+					
+					// Textos del nodo, old y new values
+				    newValue = useRetrieveUID ? (UID_REFERENCE_PREFIX+retrieveUIDValue) : String.valueOf(element.getNewValue());
+	//				if(element.getBinaryValue() != null){
+	//					/** TODO: VER QUE HACER ACA CON LOS BINARIOS EN REPLICACIÓN! */
+	//				}
+					// En el AD_Org_ID en realidad no se envia el AD_Org_ID sino el host asociado (replicationArrayPos) cargado, 
+					// dado que este es el único valor en común.  Para AD_Org_ID = 0, pasamos directamente ese valor sin mapear 
+					if ("AD_Org_ID".equalsIgnoreCase(element.getColumnName()) && !"UID=AD_Org-0".equals(newValue))
+					{
+						// Si useRetrieveUID es false, entonces UID no estará seteado.  Sin embargo, AD_Org_ID podrìa ser 0 en este caso
+						// el cual tambien es necesario omitir, dejando también 0 como valor.
+						if (!"0".equals(newValue)) 
+						{
+							Integer orgPos = ReplicationCache.map_RepArrayPos_OrgID.get(newValue.replace("UID=AD_Org-", ""));
+							if (orgPos == null)
+								throw new Exception ("No hay mapeo posible para la organización " + newValue + " en la tabla de hosts de replicación ");
+							newValue = orgPos.toString();
+						}
+					}
+					// Agrego los nodos texto al nodo newValue
+					m_replicationXMLData.append("<newValue>").append(newValue).append("</newValue>");
+					// Cierre de columna
+					m_replicationXMLData.append("</column>");
 				}
-				// Agrego los nodos texto al nodo newValue
-				m_replicationXMLData.append("<newValue>").append(newValue).append("</newValue>");
-				// Cierre de columna
-				m_replicationXMLData.append("</column>");
+	
+				// Cierre del changegroup
+				m_replicationXMLData.append("</changegroup>");
+				
+				// Enviar a replicacion, una vez por cada destino a replicar
+				for (int arrayPos = 0; arrayPos < group.getRepArray().length(); arrayPos++)
+					// En las posiciones que corresponde, se envia a replicacion (cola de eventos).  
+					// Si hubo timeOut, tambièn se reenvia donde corresponda
+					// En caso de estar reenviando todos los registros, se envia a todo el repArray (menos donde se indique sin accion)
+					if (ReplicationConstants.replicateStates.contains(group.getRepArray().charAt(arrayPos)) || 
+						  (group.isTimeOut() && ReplicationConstants.timeOutStates.contains(group.getRepArray().charAt(arrayPos))) ||
+						  (ReplicationConstants.RESEND_ALL_RECORDS && ReplicationConstants.REPLICATION_CONFIGURATION_NO_ACTION!=group.getRepArray().charAt(arrayPos)) )
+							sendToEventQueue(group, arrayPos);
 			}
-
-			// Cierre del changegroup
-			m_replicationXMLData.append("</changegroup>");
-			
-			// Enviar a replicacion, una vez por cada destino a replicar
-			for (int arrayPos = 0; arrayPos < group.getRepArray().length(); arrayPos++)
-				// En las posiciones que corresponde, se envia a replicacion (cola de eventos).  
-				// Si hubo timeOut, tambièn se reenvia donde corresponda
-				// En caso de estar reenviando todos los registros, se envia a todo el repArray (menos donde se indique sin accion)
-				if (ReplicationConstants.replicateStates.contains(group.getRepArray().charAt(arrayPos)) || 
-					  (group.isTimeOut() && ReplicationConstants.timeOutStates.contains(group.getRepArray().charAt(arrayPos))) ||
-					  (ReplicationConstants.RESEND_ALL_RECORDS && ReplicationConstants.REPLICATION_CONFIGURATION_NO_ACTION!=group.getRepArray().charAt(arrayPos)) )
-						sendToEventQueue(group, arrayPos);
-			
+			catch (Exception e) 	{
+				// TODO: esto deberia incluirse en el log de base de datos				
+				System.err.println("Error en ReplicationBuilder: " + e.getMessage());
+			}
+		
 			// Limpiar memoria cada cierto intervalo de iteraciones
 			if (i++ % 1000 == 0)
 				System.gc();
@@ -195,7 +203,7 @@ public class ReplicationBuilder extends ChangeLogXMLBuilder {
 	{
 		// estado del registro en la posicion arrayPos
 		char state = group.getRepArray().charAt(arrayPos);
-		
+
 		/* === CASOS DE ELIMINACION === */
 		if (MChangeLog.OPERATIONTYPE_Deletion.equals(group.getOperation()))
 		{
@@ -203,10 +211,10 @@ public class ReplicationBuilder extends ChangeLogXMLBuilder {
 			// Contemplando tambien casos por time out
 			if (ReplicationConstants.REPARRAY_REPLICATE_INSERT == state || ReplicationConstants.timeOutStates.contains(state) )
 				replicationHndler.appendToEventQueue(arrayPos+1, m_replicationXMLData.toString(), MChangeLog.OPERATIONTYPE_Deletion);
-			// Reintento de eliminacion
-			else if (ReplicationConstants.errorStates.contains(state))
+			// Reintento de eliminacion. Tambien valido para reenvio completo.
+			else if (ReplicationConstants.errorStates.contains(state) || ReplicationConstants.RESEND_ALL_RECORDS)
 				replicationHndler.appendToEventQueue(arrayPos+1, m_replicationXMLData.toString(), MChangeLog.OPERATIONTYPE_Deletion + state);
-			// En caso contrario, elevar excepcion
+			// En caso contrario, elevar exception
 			else
 				throw new Exception("ReplicationBuilder. Sin opType para: " + group);
 		}
@@ -229,8 +237,8 @@ public class ReplicationBuilder extends ChangeLogXMLBuilder {
 			// Si está esperando ack, y se llega a este punto, es por timeOut de la espera. Manejarlo como caso de error. Tambien valido para reenvio completo
 			else if (ReplicationConstants.timeOutStates.contains(state) || ReplicationConstants.RESEND_ALL_RECORDS)
 				replicationHndler.appendToEventQueue(arrayPos+1, m_replicationXMLData.toString(), MChangeLog.OPERATIONTYPE_InsertionModification + ReplicationConstants.REPARRAY_RETRY1);
+			// En caso contrario, informar error para este changeGroup
 			else
-				// En caso contrario, elevar excepcion
 				throw new Exception("ReplicationBuilder. Sin opType para: " + group);
 		}
 		// Actualizar el repArray para este registro, en la posición en cuestión
