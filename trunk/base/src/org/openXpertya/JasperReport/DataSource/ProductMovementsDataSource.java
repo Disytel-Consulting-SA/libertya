69676b5fdb8802513fc1b7e9de23dfa6b43f3e40
matias.cap@disytel.net <matias.cap@disytel.net@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2012-07-05 19:36:03 +0000

    Nuevo informe Movimientos por Artículo

---
+++ trunk/base/src/org/openXpertya/JasperReport/DataSource/ProductMovementsDataSource.java
@@ -0,0 +1,242 @@
+package org.openXpertya.JasperReport.DataSource;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRField;
+
+import org.openXpertya.model.MInOut;
+import org.openXpertya.model.MInventory;
+import org.openXpertya.model.MMovement;
+import org.openXpertya.model.MProductChange;
+import org.openXpertya.model.MSplitting;
+import org.openXpertya.model.MTransfer;
+import org.openXpertya.util.Env;
+import org.openXpertya.util.Msg;
+import org.openXpertya.util.Util;
+
+public class ProductMovementsDataSource extends QueryDataSource {
+
+	/** Contexto */
+	private Properties ctx;
+	
+	/** Organización */
+	private Integer orgID;
+	
+	/** Fecha desde */
+	private Timestamp dateFrom;
+	
+	/** Fecha hasta */
+	private Timestamp dateTo;
+	
+	/** Descripción inicial de Cambio de Artículo */
+	private String initProductChangeDescription;
+	
+	/** Descripción inicial de Fraccionamiento */
+	private String initSplittingDescription;
+	
+	public ProductMovementsDataSource(String trxName) {
+		super(trxName);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ProductMovementsDataSource(Properties ctx, Integer orgID, Timestamp dateFrom, Timestamp dateTo, String trxName){
+		this(trxName);
+		setCtx(ctx);
+		setOrgID(orgID);
+		setDateFrom(dateFrom);
+		setDateTo(dateTo);
+		setInitProductChangeDescription(Msg.translate(getCtx(),
+				"M_ProductChange_ID"));
+		setInitSplittingDescription(Msg.translate(getCtx(), "M_Splitting_ID"));
+	}
+
+	@Override
+	protected String getQuery() {
+		StringBuffer sql = new StringBuffer("SELECT tablename, " +
+									"				ad_org_id, " +
+									"				orgvalue, " +
+									"				orgname, " +
+									"				documentno, " +
+									"				description, " +
+									"				datetrx, " +
+									"				m_product_id, " +
+									"				qty, " +
+									"				type, " +
+									"				aditionalType, " +
+									"				c_charge_id, " +
+									"				chargename, " +
+									"				productname, " +
+									"				productvalue " +
+									"FROM v_product_movements " +
+									"WHERE docstatus IN ('CL','CO') " +
+									"		AND ad_client_id = ? ");
+		if(getOrgID() != null){
+			sql.append(" AND ad_org_id = ? ");
+		}
+		if(getDateFrom() != null){
+			sql.append(" AND datetrx >= ?::date ");
+		}
+		if(getDateTo() != null){
+			sql.append(" AND datetrx <= ?::date ");
+		}
+		sql.append(" ORDER BY productvalue, orgvalue, datetrx ");
+		return sql.toString();
+	}
+
+	@Override
+	protected Object[] getParameters() {
+		List<Object> params = new ArrayList<Object>();
+		params.add(Env.getAD_Client_ID(getCtx()));
+		if(getOrgID() != null){
+			params.add(getOrgID());
+		}
+		if(getDateFrom() != null){
+			params.add(getDateFrom());
+		}
+		if(getDateTo() != null){
+			params.add(getDateTo());
+		}
+		return params.toArray();
+	}
+	
+	@Override
+	public Object getFieldValue(JRField field) throws JRException {
+		Object value = getCurrentRecord().get(field.getName().toUpperCase());
+		if(field.getName().equalsIgnoreCase("DESCRIPTION")){
+			value = getDescriptionByTable();
+		}
+		return value;
+	}
+
+	protected String getDescriptionByTable(){
+		String tablename = (String)getCurrentRecord().get("TABLENAME");
+		String description = "";
+		if(tablename.equalsIgnoreCase(MTransfer.Table_Name)){
+			description = getTransferDescription();			
+		}
+		else if(tablename.equalsIgnoreCase(MProductChange.Table_Name)){
+			description = getProductChangeDescription();
+		}
+		else if(tablename.equalsIgnoreCase(MInOut.Table_Name)){
+			description = getInOutDescription();
+		}
+		else if(tablename.equalsIgnoreCase(MSplitting.Table_Name)){
+			description = getSplittingDescription();
+		}
+		else if(tablename.equalsIgnoreCase(MInventory.Table_Name)){
+			description = getInventoryDescription();
+		}
+		else if(tablename.equalsIgnoreCase(MMovement.Table_Name)){
+			description = getMovementDescription();
+		}
+		return description;
+	}
+	
+	protected String getTransferDescription(){
+		return JasperReportsUtil.getListName(getCtx(),
+				MTransfer.TRANSFERTYPE_AD_Reference_ID,
+				(String) getCurrentRecord().get("TYPE"))
+				+ " - "
+				+ JasperReportsUtil.getListName(getCtx(),
+						MTransfer.MOVEMENTTYPE_AD_Reference_ID,
+						(String) getCurrentRecord().get("ADITIONALTYPE"))
+				+ (Util.isEmpty((String) getCurrentRecord().get("CHARGENAME"),
+						true) ? "" : " - "
+						+ (String) getCurrentRecord().get("CHARGENAME"))
+				+ (Util.isEmpty((String) getCurrentRecord().get("DESCRIPTION"),
+						true) ? "" : " - "
+						+ (String) getCurrentRecord().get("DESCRIPTION"));
+	}
+	
+	protected String getProductChangeDescription(){
+		return getInitProductChangeDescription()
+				+ (Util.isEmpty((String) getCurrentRecord().get("DESCRIPTION"),
+						true) ? "" : " - "
+							+ (String) getCurrentRecord().get("DESCRIPTION"));
+	}
+	
+	protected String getInOutDescription(){
+		return (String) getCurrentRecord().get("TYPE")
+				+ (Util.isEmpty((String) getCurrentRecord().get("DESCRIPTION"),
+						true) ? "" : " - "
+						+ (String) getCurrentRecord().get("DESCRIPTION"));
+	}
+	
+	protected String getSplittingDescription(){
+		return getInitSplittingDescription()
+				+ (Util.isEmpty((String) getCurrentRecord().get("DESCRIPTION"),
+						true) ? "" : " - "
+						+ (String) getCurrentRecord().get("DESCRIPTION")); 
+	}
+	
+	protected String getInventoryDescription(){
+		return getInitSplittingDescription()
+				+ (Util.isEmpty((String) getCurrentRecord().get("CHARGENAME"),
+						true) ? "" : " - "
+						+ (String) getCurrentRecord().get("CHARGENAME"))
+				+ (Util.isEmpty((String) getCurrentRecord().get("DESCRIPTION"),
+						true) ? "" : " - "
+						+ (String) getCurrentRecord().get("DESCRIPTION"));
+	}
+	
+	protected String getMovementDescription(){
+		return (String) getCurrentRecord().get("TYPE")
+				+ (Util.isEmpty((String) getCurrentRecord().get("DESCRIPTION"),
+						true) ? "" : " - "
+						+ (String) getCurrentRecord().get("DESCRIPTION"));
+	}
+	
+	protected void setCtx(Properties ctx) {
+		this.ctx = ctx;
+	}
+
+	protected Properties getCtx() {
+		return ctx;
+	}
+
+	protected void setOrgID(Integer orgID) {
+		this.orgID = orgID;
+	}
+
+	protected Integer getOrgID() {
+		return orgID;
+	}
+
+	protected void setDateFrom(Timestamp dateFrom) {
+		this.dateFrom = dateFrom;
+	}
+
+	protected Timestamp getDateFrom() {
+		return dateFrom;
+	}
+
+	protected void setDateTo(Timestamp dateTo) {
+		this.dateTo = dateTo;
+	}
+
+	protected Timestamp getDateTo() {
+		return dateTo;
+	}
+
+	protected void setInitProductChangeDescription(
+			String initProductChangeDescription) {
+		this.initProductChangeDescription = initProductChangeDescription;
+	}
+
+	protected String getInitProductChangeDescription() {
+		return initProductChangeDescription;
+	}
+
+	protected void setInitSplittingDescription(String initSplittingDescription) {
+		this.initSplittingDescription = initSplittingDescription;
+	}
+
+	protected String getInitSplittingDescription() {
+		return initSplittingDescription;
+	}
+
+}
--- trunk/data/core/upgrade_from_11.10/preinstall_from_11.10.sql
+++ trunk/data/core/upgrade_from_11.10/preinstall_from_11.10.sql
@@ -2761,3 +2761,50 @@ BEGIN
 END
 $BODY$
 LANGUAGE 'plpgsql' VOLATILE;
+
+-- 20120705-0900 Vista para el informe de movimientos por artículo
+CREATE OR REPLACE VIEW v_product_movements AS 
+ SELECT m.tablename, m.ad_client_id, m.ad_org_id, o.value AS orgvalue, o.name AS orgname, m.documentno, m.docstatus, m.description, m.datetrx, m.m_product_id, m.qty, m.type, m.aditionaltype, m.c_charge_id, m.chargename, p.name AS productname, p.value AS productvalue
+   FROM (        (        (        (        (        (        (         SELECT 'M_Transfer' AS tablename, t.ad_client_id, t.ad_org_id, t.documentno, t.docstatus, t.description, t.datetrx, tl.m_product_id, tl.qty, t.transfertype AS type, t.movementtype AS aditionaltype, c.c_charge_id, c.name AS chargename
+                                                                   FROM m_transfer t
+                                                              JOIN m_transferline tl ON tl.m_transfer_id = t.m_transfer_id
+                                                         LEFT JOIN c_charge c ON c.c_charge_id = t.c_charge_id
+                                                        UNION ALL 
+                                                                 SELECT 'M_ProductChange' AS tablename, pc.ad_client_id, pc.ad_org_id, pc.documentno, pc.docstatus, pc.description, pc.datetrx, pc.m_product_id, pc.productqty AS qty, NULL::unknown AS type, NULL::unknown AS aditionaltype, NULL::unknown AS c_charge_id, NULL::unknown AS chargename
+                                                                   FROM m_productchange pc)
+                                                UNION ALL 
+                                                         SELECT 'M_ProductChange' AS tablename, pc.ad_client_id, pc.ad_org_id, pc.documentno, pc.docstatus, pc.description, pc.datetrx, pc.m_product_to_id AS m_product_id, pc.productqty * (-1)::numeric AS qty, NULL::unknown AS type, NULL::unknown AS aditionaltype, NULL::unknown AS c_charge_id, NULL::unknown AS chargename
+                                                           FROM m_productchange pc)
+                                        UNION ALL 
+                                                 SELECT 'M_InOut' AS tablename, io.ad_client_id, io.ad_org_id, io.documentno, io.docstatus, io.description, io.movementdate AS datetrx, iol.m_product_id, iol.movementqty AS qty, dt.name AS type, NULL::unknown AS aditionaltype, NULL::unknown AS c_charge_id, NULL::unknown AS chargename
+                                                   FROM m_inout io
+                                              JOIN m_inoutline iol ON iol.m_inout_id = io.m_inout_id
+                                         JOIN c_doctype dt ON dt.c_doctype_id = io.c_doctype_id)
+                                UNION ALL 
+                                         SELECT 'M_Splitting' AS tablename, s.ad_client_id, s.ad_org_id, s.documentno, s.docstatus, s.comments AS description, s.datetrx, s.m_product_id, s.productqty * (-1)::numeric AS qty, NULL::unknown AS type, NULL::unknown AS aditionaltype, NULL::unknown AS c_charge_id, NULL::unknown AS chargename
+                                           FROM m_splitting s)
+                        UNION ALL 
+                                 SELECT 'M_Splitting' AS tablename, s.ad_client_id, s.ad_org_id, s.documentno, s.docstatus, s.comments AS description, s.datetrx, sl.m_product_to_id AS m_product_id, sl.productqty AS qty, NULL::unknown AS type, NULL::unknown AS aditionaltype, NULL::unknown AS c_charge_id, NULL::unknown AS chargename
+                                   FROM m_splitting s
+                              JOIN m_splittingline sl ON sl.m_splitting_id = s.m_splitting_id)
+                UNION ALL 
+                         SELECT 'M_Inventory' AS tablename, i.ad_client_id, i.ad_org_id, i.documentno, i.docstatus, i.description, i.movementdate AS datetrx, il.m_product_id, il.qtycount AS qty, dt.name AS type, i.inventorykind AS aditionaltype, c.c_charge_id, c.name AS chargename
+                           FROM m_inventory i
+                      JOIN m_inventoryline il ON i.m_inventory_id = il.m_inventory_id
+                 JOIN c_doctype dt ON dt.c_doctype_id = i.c_doctype_id
+            LEFT JOIN c_charge c ON c.c_charge_id = i.c_charge_id
+           WHERE NOT (i.m_inventory_id IN ( SELECT DISTINCT m_transfer.m_inventory_id
+                    FROM m_transfer)) AND NOT (i.m_inventory_id IN ( SELECT DISTINCT m_productchange.m_inventory_id
+                    FROM m_productchange)) AND NOT (i.m_inventory_id IN ( SELECT DISTINCT m_productchange.void_inventory_id
+                    FROM m_productchange)) AND NOT (i.m_inventory_id IN ( SELECT DISTINCT s.m_inventory_id
+                    FROM m_splitting s)) AND NOT (i.m_inventory_id IN ( SELECT DISTINCT s.void_inventory_id
+                    FROM m_splitting s)))
+        UNION ALL 
+                 SELECT 'M_Movement' AS tablename, m.ad_client_id, m.ad_org_id, m.documentno, m.docstatus, m.description, m.movementdate AS datetrx, ml.m_product_id, ml.movementqty AS qty, dt.name AS type, NULL::unknown AS aditionaltype, NULL::unknown AS c_charge_id, NULL::unknown AS chargename
+                   FROM m_movement m
+              JOIN m_movementline ml ON ml.m_movement_id = m.m_movement_id
+         JOIN c_doctype dt ON dt.c_doctype_id = m.c_doctype_id) m
+   JOIN m_product p ON p.m_product_id = m.m_product_id
+   JOIN ad_org o ON o.ad_org_id = m.ad_org_id;
+
+ALTER TABLE v_product_movements OWNER TO libertya;
