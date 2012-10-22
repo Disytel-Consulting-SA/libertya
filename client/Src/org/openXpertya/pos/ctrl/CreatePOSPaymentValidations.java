b18c97bcf4911514c492b73a66b4dae1b1ed53a7
matias.cap <matias.cap@disytel.net>
2012-10-22 15:50:08 +0000

    Nueva clase para extensiones de validaciones en la creación de los cobros en el TPV. Mejora al flag de sobraCheques, no se estaba teniendo enn cuenta el efectivo, prioritario en el tema del cambio.

---
+++ client/Src/org/openXpertya/pos/ctrl/CreatePOSPaymentValidations.java
@@ -0,0 +1,10 @@
+package org.openXpertya.pos.ctrl;
+
+import org.openXpertya.pos.model.Order;
+
+public class CreatePOSPaymentValidations {
+	
+	public void validatePayments(PoSOnline posOnline, Order order) {
+		
+	}	
+}
