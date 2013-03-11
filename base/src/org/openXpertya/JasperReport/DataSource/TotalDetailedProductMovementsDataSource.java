5277c72fb0de8b3ecd3758df824438b76e0ee288
matias.cap <matias.cap@disytel.net>
2013-03-11 15:50:33 +0000

    Nuevo reporte Movimientos de artículos detallado

---
+++ base/src/org/openXpertya/JasperReport/DataSource/TotalDetailedProductMovementsDataSource.java
@@ -0,0 +1,33 @@
+package org.openXpertya.JasperReport.DataSource;
+
+import java.sql.Timestamp;
+import java.util.Properties;
+
+public class TotalDetailedProductMovementsDataSource extends
+		DetailedProductMovementsDataDource {
+
+	public TotalDetailedProductMovementsDataSource(String trxName) {
+		super(trxName);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public TotalDetailedProductMovementsDataSource(String trxName, Properties ctx, Integer productID, Integer warehouseID, Timestamp dateFrom, Timestamp dateTo) {
+		super(trxName, ctx, productID, warehouseID, dateFrom, dateTo);
+		// TODO Auto-generated constructor stub
+	}
+	
+	protected String getSQLOrderBy(){
+		return "";
+	}
+	
+	@Override
+	protected String getQuery() {
+		StringBuffer sql = new StringBuffer("SELECT receiptvalue, doctypename, sum(qty) as qty FROM (");
+		sql.append(super.getQuery());
+		sql.append(") as t ");
+		sql.append(" GROUP BY receiptvalue, doctypename ");
+		sql.append(" ORDER BY receiptvalue, doctypename ");
+		return sql.toString();
+	}
+
+}
