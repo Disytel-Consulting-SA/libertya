1fd952e6e30eaa7655aa451a4d28bbee047498a1
matias.cap <matias.cap@disytel.net>
2013-03-25 17:07:19 +0000

    Fixes a la consulta del reporte de ventas por línea de artículo

--- base/src/org/openXpertya/JasperReport/DataSource/ProductLinesSalesDataSource.java
+++ base/src/org/openXpertya/JasperReport/DataSource/ProductLinesSalesDataSource.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.openXpertya.model.MDocType;
 import org.openXpertya.util.Util;
 
 public class ProductLinesSalesDataSource extends QueryDataSource {
@@ -37,14 +38,15 @@ public class ProductLinesSalesDataSource extends QueryDataSource {
 	@Override
 	protected String getQuery() {
 		StringBuffer sql = new StringBuffer("select pl.m_product_lines_id, pl.value as product_line_value, pl.name as product_line_name, coalesce(s.cost,0.00) as cost, coalesce(s.sales,0.00) as sales from m_product_lines as pl left join ( ");
-		sql.append("select pl.m_product_lines_id, pl.value as product_line_value, pl.name as product_line_name, sum(costprice*qtyentered) as cost, sum((priceentered-documentdiscountamt)*qtyentered) as sales " +
+		sql.append("select pl.m_product_lines_id, pl.value as product_line_value, pl.name as product_line_name, sum(il.costprice * il.qtyentered) as cost, sum((il.priceentered * il.qtyentered) - il.documentdiscountamt) as sales " +
 					 "from c_invoiceline as il " +
 					 "inner join c_invoice as i on i.c_invoice_id = il.c_invoice_id " +
+					 "inner join c_doctype as dt on dt.c_doctype_id = i.c_doctypetarget_id "+
 					 "inner join m_product as p on p.m_product_id = il.m_product_id " +
 					 "inner join m_product_category as pc on pc.m_product_category_id = p.m_product_category_id " +
 					 "inner join m_product_gamas as pg on pg.m_product_gamas_id = pc.m_product_gamas_id " +
 					 "inner join m_product_lines as pl on pl.m_product_lines_id = pg.m_product_lines_id " +
-					 "where il.ad_org_id = ? AND pl.isactive = 'Y' AND date_trunc('day',i.dateinvoiced) >= date_trunc('day',?::date) AND date_trunc('day',i.dateinvoiced) <= date_trunc('day',?::date) ");
+					 "where il.ad_org_id = ? AND pl.isactive = 'Y' AND i.docstatus IN ('CO','CL') AND dt.docbasetype = '"+MDocType.DOCBASETYPE_ARInvoice+"' AND dt.doctypekey NOT IN ('RCI') AND date_trunc('day',i.dateinvoiced) >= date_trunc('day',?::date) AND date_trunc('day',i.dateinvoiced) <= date_trunc('day',?::date) ");
 		if(!Util.isEmpty(getProductLinesID(), true)){
 			sql.append(" AND pl.m_product_lines_id = ? ");
 		}
