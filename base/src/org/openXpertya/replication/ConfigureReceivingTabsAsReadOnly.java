04d9c1ac95b155ddfa60c8406b39734c6dd79957
federico.cristina <118191354+fcristina77@users.noreply.github.com>
2013-05-06 12:32:17 +0000

    Proceso para actualizar los campos IsReadOnly/IsInsertRecord de AD_Tab en funcion de la configuración de replicación a fin de inhabilitar la creación de registros en tablas donde sólo debe recibirse información de otros hosts (cont.)

--- base/src/org/openXpertya/replication/ConfigureReceivingTabsAsReadOnly.java
+++ base/src/org/openXpertya/replication/ConfigureReceivingTabsAsReadOnly.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 import org.openXpertya.OpenXpertya;
+import org.openXpertya.model.MPreference;
 import org.openXpertya.model.X_AD_Tab;
 import org.openXpertya.util.CLogger;
 import org.openXpertya.util.DB;
@@ -13,16 +14,31 @@ import org.openXpertya.util.Env;
  * En función de la configuración por tabla en AD_TableReplication, pasar a read-only = 'Y' (o isInsertRecord = 'N')
  * las pestañas correspondientes a las tablas marcadas como "solo recepción".
  * 
- * IMPORTANTE: Configurar la variable changeField según se desee setear el campo isReadOnly o el campo isInertRecord
+ * =============================================================================================
+ * IMPORTANTE! Utiliza la conexión a base de datos especificada en $OXP_HOME/Libertya.properties
+ * =============================================================================================
+ * 
+ * ========================================================================================================
+ * IMPORTANTE: Por defecto aplica la lógica de read-only, pero por parámetro se puede modificar esta opción
+ * ========================================================================================================
+ * 
+ * =======================================================================================================================
+ * IMPORTANTE: La nómina de tabs a omitir cambios debe almacenarse separada por comas en AD_Preference, ver TAB_EXCEPTIONS
+ * =======================================================================================================================
  */
 
 public class ConfigureReceivingTabsAsReadOnly extends AbstractReplicationProcess {
 
+	/** Opcion: Pasar a read only = Y las pestañas */
 	static final String FIELD_READONLY		= "IsReadOnly";
+	/** Opcion: Pasar a insert recrod = N las pestañas */
 	static final String FIELD_INSERTRECORD	= "IsInsertRecord";
 	
-	/** === CAMBIAR PARA ESPECIFICAR SI SE QUIERE UTILIZAR IsInsertRecord en lugar de IsReadOnly === */
+	/** === Lógica a utilizar: FIELD_READONLY o FIELD_INSERTRECORD === */
 	static String changeField = FIELD_READONLY;
+	
+	/** Entrada en AD_Preference para especificar la nómina pestañas que NO deben modificarse (AD_Tab_IDs SEPARADOS POR ,) */
+	protected static final String TAB_EXCEPTIONS 	= "InmutableReceivingTabsList";
 	
 	@Override
 	protected String getProcessName() {
@@ -31,6 +47,12 @@ public class ConfigureReceivingTabsAsReadOnly extends AbstractReplicationProcess
 	
 	public static void main(String[] args) 
 	{
+		if (args.length > 0) {
+			changeField = args[0];
+			if (!FIELD_READONLY.equals(changeField) && !FIELD_INSERTRECORD.equals(changeField))
+				showHelp("El parámetro de tipo de acción debe ser " + FIELD_READONLY + " o " + FIELD_INSERTRECORD);
+		}
+		
 	  	// OXP_HOME seteada?
 	  	String oxpHomeDir = System.getenv("OXP_HOME"); 
 	  	if (oxpHomeDir == null)
@@ -85,7 +107,7 @@ public class ConfigureReceivingTabsAsReadOnly extends AbstractReplicationProcess
 				continue;
 			// Si se llegó a este punto es porque para la tabla dada, solo se recibe. 
 			// Deben marcarse como solo lectura las pestañas en cuestión (que todavia no esten marcadas como solo lectura)
-			PreparedStatement pstmtTab = DB.prepareStatement("SELECT AD_Tab_ID FROM AD_Tab WHERE " + getWhereClause() + " AND AD_Table_ID = " + tableID);
+			PreparedStatement pstmtTab = DB.prepareStatement("SELECT AD_Tab_ID FROM AD_Tab WHERE " + getWhereClause() + " AND AD_Table_ID = " + tableID + getTabExceptions());
 			ResultSet rsTab = pstmtTab.executeQuery();
 			// Actualizar cada tab que referencie a la tabla en cuestion
 			while (rsTab.next()) {
@@ -97,9 +119,7 @@ public class ConfigureReceivingTabsAsReadOnly extends AbstractReplicationProcess
 					System.out.println("Modificado valor " + changeField + " a " + getNewValue() + " para tab: " + aTab.getName() + " (" + aTab.getAD_Tab_ID() + "), tabla: " + tableName + " (" + tableID + ").");
 			}
 		}
-		
-		return "FINALIZADO";
-		
+		return "FINALIZADO";		
 	}
 	
 	/** Retorna la parte del query correspondiente */
@@ -114,6 +134,14 @@ public class ConfigureReceivingTabsAsReadOnly extends AbstractReplicationProcess
 		if (FIELD_READONLY.equals(changeField))
 			return "Y";
 		return "N";
+	}
+	
+	/** Retorna la nómina de pestañas a no modificar (a partir de la AD_Preference TAB_EXCEPTIONS), en formato SQL: tabID, tabID, tabID */
+	public String getTabExceptions() {
+		String values = MPreference.searchCustomPreferenceValue(TAB_EXCEPTIONS, 0, 0, 0, false);
+		if (values==null || values.length()==0)
+			return "";
+		return " AND AD_Tab_ID NOT IN (" + values + ")";
 	}
 	
 }
