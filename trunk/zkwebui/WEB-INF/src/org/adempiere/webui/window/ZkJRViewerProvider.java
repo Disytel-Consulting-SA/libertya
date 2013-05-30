3baed9c9805f3b9817c57be64f38034a4e3b5c4d
federico.cristina@disytel.net <federico.cristina@disytel.net@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2013-05-30 14:27:47 +0000

    LYWeb: Incluir JasperReports.  Se quitó una librería no utilizada/referenciada en el código (jdt-compiler-3.1.1.jar) la cual impedía la correcta carga de LYWS.  Se incluye en la configuración de inicio de jboss la libreria completa JasperReports.jar, la cual es requerida por LYWeb.  Por el momento se previsualiza siempre bajo LYWeb (ver MJasperReport)

--- trunk/zkwebui/WEB-INF/src/org/adempiere/webui/window/ZkJRViewerProvider.java
+++ trunk/zkwebui/WEB-INF/src/org/adempiere/webui/window/ZkJRViewerProvider.java
@@ -3,19 +3,22 @@ package org.adempiere.webui.window;
 //import net.sf.jasperreports.engine.JRException;
 //import net.sf.jasperreports.engine.JasperPrint;
 
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperPrint;
+
 import org.adempiere.webui.component.Window;
 import org.adempiere.webui.session.SessionManager;
 import org.openXpertya.JasperReport.JRViewerProvider;
 
-public class ZkJRViewerProvider /*implements JRViewerProvider*/ {
+public class ZkJRViewerProvider implements JRViewerProvider {
 
-//	public void openViewer(JasperPrint jasperPrint, String title)
-//			throws JRException {
-//		Window viewer = new ZkJRViewer(jasperPrint, title);
-//		
-//		viewer.setAttribute(Window.MODE_KEY, Window.MODE_EMBEDDED);
-//		viewer.setAttribute(Window.INSERT_POSITION_KEY, Window.INSERT_NEXT);
-//		SessionManager.getAppDesktop().showWindow(viewer);
-//	}
+	public void openViewer(JasperPrint jasperPrint, String title)
+			throws JRException {
+		Window viewer = new ZkJRViewer(jasperPrint, title);
+		
+		viewer.setAttribute(Window.MODE_KEY, Window.MODE_EMBEDDED);
+		viewer.setAttribute(Window.INSERT_POSITION_KEY, Window.INSERT_NEXT);
+		SessionManager.getAppDesktop().showWindow(viewer);
+	}
 
 }
