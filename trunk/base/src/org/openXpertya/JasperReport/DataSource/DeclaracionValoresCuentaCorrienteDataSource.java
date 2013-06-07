58f9d58a4cb92d24a98b60d122a963be92f79930
matias.cap@disytel.net <matias.cap@disytel.net@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2013-06-07 21:00:41 +0000

    Fixes al reporte y vista de declaración de valores porque mostraba las facturas manuales como cuenta corriente pero en realidad se habían pagado en el día

--- trunk/base/src/org/openXpertya/JasperReport/DataSource/DeclaracionValoresCuentaCorrienteDataSource.java
+++ trunk/base/src/org/openXpertya/JasperReport/DataSource/DeclaracionValoresCuentaCorrienteDataSource.java
@@ -23,7 +23,12 @@ public class DeclaracionValoresCuentaCorrienteDataSource extends
 	protected String getQuery() {
 		// Monto pendiente de la facturas
 		StringBuffer superSql = new StringBuffer("SELECT invoice_documentno, invoice_grandtotal, open as ingreso, egreso, open as total   FROM (");
-		StringBuffer sql = new StringBuffer("SELECT dv.*,initialcurrentaccountamt as open  FROM c_pos_declaracionvalores_v as dv INNER JOIN c_invoice as i ON i.c_invoice_id = dv.doc_id WHERE ");
+		StringBuffer sql = new StringBuffer("SELECT dv.*,invoice_grandtotal-coalesce(ds.amount,0) as open  " +
+											"FROM c_pos_declaracionvalores_v as dv INNER JOIN (select c_invoice_id as alloc_invoice_id, c_posjournal_id as alloc_journal_id, sum(amount) as amount " +
+																								"from c_allocationhdr as ah " +
+																								"inner join c_allocationline as al on al.c_allocationhdr_id = ah.c_allocationhdr_id " +
+																								"where ah.isactive = 'Y' " +
+																								"group by c_invoice_id, c_posjournal_id) as ds on ds.alloc_invoice_id = dv.c_invoice_id and dv.c_posjournal_id = ds.alloc_journal_id WHERE ");
 		sql.append(getStdWhereClause(true,"dv"));
 		superSql.append(sql);
 		superSql.append(") as inv ");
