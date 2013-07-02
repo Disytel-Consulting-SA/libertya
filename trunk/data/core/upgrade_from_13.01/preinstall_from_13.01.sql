71490d6266f1107480cf9b1f1f0ec21dea19b6d8
federico.cristina@disytel.net <federico.cristina@disytel.net@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2013-07-02 13:39:27 +0000

    LYWeb: Nuevas vistas requeridas para el InfoProductPanel

--- trunk/data/core/upgrade_from_13.01/preinstall_from_13.01.sql
+++ trunk/data/core/upgrade_from_13.01/preinstall_from_13.01.sql
@@ -3615,3 +3615,34 @@ UPDATE ad_system SET dummy = (SELECT addcolumnifnotexists('t_estadodecuenta','gr
 UPDATE ad_system SET dummy = (SELECT addcolumnifnotexists('t_estadodecuenta','paidamtmulticurrency', 'numeric'));
 UPDATE ad_system SET dummy = (SELECT addcolumnifnotexists('t_estadodecuenta','openamtmulticurrency', 'numeric'));
 UPDATE ad_system SET dummy = (SELECT addcolumnifnotexists('t_estadodecuenta','conversionrate', 'numeric'));
+
+-- 20130702-1035 LYWeb: Nuevas vistas requeridas para el InfoProductPanel
+CREATE OR REPLACE VIEW m_product_stock_v AS 
+ SELECT ms.isactive, ms.created, ms.createdby, ms.updated, ms.updatedby, mp.value, mp.help, ms.qtyonhand - ms.qtyreserved AS qtyavailable, ms.qtyonhand, ms.qtyreserved, mp.description, mw.name AS warehouse, mw.m_warehouse_id, mw.ad_client_id, mw.ad_org_id, mp.documentnote
+   FROM m_storage ms
+   JOIN m_product mp ON ms.m_product_id = mp.m_product_id
+   JOIN m_locator ml ON ms.m_locator_id = ml.m_locator_id
+   JOIN m_warehouse mw ON ml.m_warehouse_id = mw.m_warehouse_id
+  ORDER BY mw.name;
+
+CREATE OR REPLACE VIEW m_product_substituterelated_v AS 
+ SELECT s.ad_client_id, s.ad_org_id, s.isactive, s.created, s.createdby, s.updated, s.updatedby, s.m_product_id, s.substitute_id, 'S' AS rowtype, mp.name, sum(ms.qtyonhand - ms.qtyreserved) AS qtyavailable, sum(ms.qtyonhand) AS qtyonhand, sum(ms.qtyreserved) AS qtyreserved, round(max(mpr.pricestd), 0) AS pricestd, mpr.m_pricelist_version_id, mw.m_warehouse_id, org.name AS orgname
+   FROM m_substitute s
+   JOIN m_storage ms ON ms.m_product_id = s.substitute_id
+   JOIN m_product mp ON ms.m_product_id = mp.m_product_id
+   JOIN m_locator ml ON ms.m_locator_id = ml.m_locator_id
+   JOIN m_warehouse mw ON ml.m_warehouse_id = mw.m_warehouse_id
+   JOIN m_productprice mpr ON ms.m_product_id = mpr.m_product_id
+   JOIN ad_org org ON org.ad_org_id = mw.ad_org_id
+  GROUP BY s.ad_client_id, s.ad_org_id, s.isactive, s.created, s.createdby, s.updated, s.updatedby, s.m_product_id, s.substitute_id, mw.m_warehouse_id, mpr.m_pricelist_version_id, org.name, mp.name
+UNION 
+ SELECT r.ad_client_id, r.ad_org_id, r.isactive, r.created, r.createdby, r.updated, r.updatedby, r.m_product_id, r.relatedproduct_id AS substitute_id, 'R' AS rowtype, mp.name, sum(ms.qtyonhand - ms.qtyreserved) AS qtyavailable, sum(ms.qtyonhand) AS qtyonhand, sum(ms.qtyreserved) AS qtyreserved, round(max(mpr.pricestd), 0) AS pricestd, mpr.m_pricelist_version_id, mw.m_warehouse_id, org.name AS orgname
+   FROM m_relatedproduct r
+   JOIN m_storage ms ON ms.m_product_id = r.relatedproduct_id
+   JOIN m_product mp ON ms.m_product_id = mp.m_product_id
+   JOIN m_locator ml ON ms.m_locator_id = ml.m_locator_id
+   JOIN m_warehouse mw ON ml.m_warehouse_id = mw.m_warehouse_id
+   JOIN m_productprice mpr ON ms.m_product_id = mpr.m_product_id
+   JOIN ad_org org ON org.ad_org_id = mw.ad_org_id
+  GROUP BY r.ad_client_id, r.ad_org_id, r.isactive, r.created, r.createdby, r.updated, r.updatedby, r.m_product_id, r.relatedproduct_id, mw.m_warehouse_id, mpr.m_pricelist_version_id, org.name, mp.name;
+
