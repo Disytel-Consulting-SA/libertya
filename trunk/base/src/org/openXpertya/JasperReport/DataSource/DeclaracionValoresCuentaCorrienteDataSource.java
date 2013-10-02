0abd4747c6f4b532e9939bc297f4223beb8ca288
matias.cap@disytel.net <matias.cap@disytel.net@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2013-10-02 18:59:15 +0000

    Fix para que muestre los comprobantes en cuenta corriente en el informe de declaración de valores

--- trunk/base/src/org/openXpertya/JasperReport/DataSource/DeclaracionValoresCuentaCorrienteDataSource.java
+++ trunk/base/src/org/openXpertya/JasperReport/DataSource/DeclaracionValoresCuentaCorrienteDataSource.java
@@ -24,12 +24,12 @@ public class DeclaracionValoresCuentaCorrienteDataSource extends
 		// Monto pendiente de la facturas
 		StringBuffer superSql = new StringBuffer("SELECT invoice_documentno, invoice_grandtotal, open as ingreso, egreso, open as total   FROM (");
 		StringBuffer sql = new StringBuffer("SELECT dv.*,invoice_grandtotal-coalesce(ds.amount,0) as open  " +
-											"FROM c_pos_declaracionvalores_v as dv INNER JOIN (select c_invoice_id as alloc_invoice_id, c_posjournal_id as alloc_journal_id, sum(amount) as amount " +
+											"FROM c_pos_declaracionvalores_v as dv LEFT JOIN (select c_invoice_id as alloc_invoice_id, c_posjournal_id as alloc_journal_id, sum(amount) as amount " +
 																								"from c_allocationhdr as ah " +
 																								"inner join c_allocationline as al on al.c_allocationhdr_id = ah.c_allocationhdr_id " +
 																								"where ah.isactive = 'Y' " +
 																								"group by c_invoice_id, c_posjournal_id) as ds on ds.alloc_invoice_id = dv.c_invoice_id and dv.c_posjournal_id = ds.alloc_journal_id WHERE ");
-		sql.append(getStdWhereClause(true,"dv"));
+		sql.append(getStdWhereClause(true,"dv",true,false));
 		superSql.append(sql);
 		superSql.append(") as inv ");
 		superSql.append(" WHERE (open > 0) ");
