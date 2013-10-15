3e9d6478881a34b86bfdbc1c0b3c9d439f35c4ad
matias.cap <matias.cap@disytel.net>
2013-10-15 16:32:47 +0000

    Nuevo proceso para copiar parámetros de un proceso a otro en botón de la ventana de procesos. Incorporación de columna a la vista de percepciones para informe relacionado.

---
+++ base/src/org/openXpertya/process/CopyProcessParameters.java
@@ -0,0 +1,60 @@
+package org.openXpertya.process;
+
+import java.math.BigDecimal;
+
+import org.openXpertya.model.MProcess;
+import org.openXpertya.model.MProcessPara;
+import org.openXpertya.model.PO;
+import org.openXpertya.util.CLogger;
+import org.openXpertya.util.Util;
+
+public class CopyProcessParameters extends SvrProcess {
+
+	/** Parámetros de proceso */
+	private MProcess process;
+	
+	@Override
+	protected void prepare() {
+		ProcessInfoParameter[] para = getParameter();
+		String name;
+		Integer processID = 0;
+        for( int i = 0;i < para.length;i++ ) {
+        	name = para[ i ].getParameterName();
+
+            if( name.equals( "AD_Process_ID" )) {
+                processID = (( BigDecimal )para[ i ].getParameter()).intValue();
+            }
+        }
+        if(!Util.isEmpty(processID, true)){
+        	setProcess(MProcess.get(getCtx(), processID, get_TrxName()));
+        }
+	}
+
+	@Override
+	protected String doIt() throws Exception {
+		if(getProcess() == null){
+        	throw new Exception("Process not exist");
+        }
+		// Obtengo los parámetros a copiar del proceso parámetro
+		MProcessPara[] paramsSrc = getProcess().getParameters();
+		MProcessPara newParam;
+		for (int i = 0; i < paramsSrc.length; i++) {
+			newParam = new MProcessPara(getCtx(), 0, get_TrxName());
+			PO.copyValues(paramsSrc[i], newParam);
+			newParam.setAD_Process_ID(getRecord_ID());
+			if(!newParam.save()){
+				throw new Exception(CLogger.retrieveErrorAsString());
+			}
+		}
+		return "@CopiedParameters@: "+paramsSrc.length;
+	}
+
+	private MProcess getProcess() {
+		return process;
+	}
+
+	private void setProcess(MProcess process) {
+		this.process = process;
+	}
+
+}
