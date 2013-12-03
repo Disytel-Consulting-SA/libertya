d6f628816b1ddb16ef422fa04f21b0f5b09db032
federico.cristina@disytel.net <federico.cristina@disytel.net@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2013-12-03 19:08:14 +0000

    Replicacion. Cliente de replicacion, apoyado en las librerias CORE de libertya y en librerías cliente de LYWS.

---
+++ trunk/utils/replicacion/query.sh
@@ -0,0 +1,1 @@
+psql -h localhost -U libertya -d libertya_prod -c "$1"
---
+++ trunk/utils/replicacion/showPendingRecords.sh
@@ -0,0 +1,3 @@
+./query.sh "SELECT * FROM replication_record_count(1010016, 'includeinreplication=''Y''')"
+./query.sh "SELECT * FROM replication_record_count(0, 'includeinreplication=''Y''')"
+
