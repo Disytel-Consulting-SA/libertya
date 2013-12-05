fa7e9022ac60bdbb2d2c1fc2282613318dd7c8ae
matias.cap@disytel.net <matias.cap@disytel.net@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2013-12-05 16:14:26 +0000

    Fixes varios a cálculo de percepciones por errores en totales de notas de crédito.

--- trunk/base/src/org/openXpertya/model/MBPartnerPadronBsAs.java
+++ trunk/base/src/org/openXpertya/model/MBPartnerPadronBsAs.java
@@ -7,6 +7,7 @@ import java.sql.Timestamp;
 import java.util.Properties;
 
 import org.openXpertya.util.DB;
+import org.openXpertya.util.Util;
 
 public class MBPartnerPadronBsAs extends X_C_BPartner_Padron_BsAs {
 
@@ -37,16 +38,52 @@ public class MBPartnerPadronBsAs extends X_C_BPartner_Padron_BsAs {
 					 " WHERE bp.c_bpartner_id = ? " +
 					 "			AND alta_baja NOT IN ('B') "+
 					 "			AND padrontype = '"+padronType+"'"+
-					 "			AND ?::date between fecha_desde AND fecha_hasta " +
+					 "			AND ?::date between fecha_desde::date AND fecha_hasta::date " +
 					 " ORDER BY p.updated DESC" +
 					 " LIMIT 1 ";
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		BigDecimal perc = null;
 		try {
-			ps = DB.prepareStatement(sql, trxName);
+			ps = DB.prepareStatement(sql, trxName, true);
 			ps.setInt(1, bpartnerID);
 			ps.setTimestamp(2, date);
+			rs = ps.executeQuery();
+			if(rs.next()){
+				perc = rs.getBigDecimal(numericColumnName);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally{
+			try {
+				if(rs != null)rs.close();
+				if(ps != null)ps.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return perc;
+	}
+	
+	public static BigDecimal getBPartnerPerc(String numericColumnName, String taxID, Timestamp date, String padronType, String trxName){
+		if(Util.isEmpty(taxID, true)){
+			return null;
+		}
+		String taxIDNoScripts = taxID.replace("-", "").trim();
+		String sql = " SELECT "+numericColumnName+
+					 " FROM "+Table_Name+" as p "+
+					 " WHERE p.cuit = '" +taxIDNoScripts+"' " +
+					 "			AND alta_baja NOT IN ('B') "+
+					 "			AND padrontype = '"+padronType+"'"+
+					 "			AND ?::date between fecha_desde::date AND fecha_hasta::date " +
+					 " ORDER BY p.updated DESC" +
+					 " LIMIT 1 ";
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		BigDecimal perc = null;
+		try {
+			ps = DB.prepareStatement(sql, trxName, true);
+			ps.setTimestamp(1, date);
 			rs = ps.executeQuery();
 			if(rs.next()){
 				perc = rs.getBigDecimal(numericColumnName);
