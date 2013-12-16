69c7885b2965f1621dd21ea508bda7704ec5eab9
matias.cap <matias.cap@disytel.net>
2013-12-16 12:21:27 +0000

    Incorporación de parámetro y columna de línea de artículo al reporte de Detalle de movimientos valorizados

--- client/Src/org/openXpertya/JasperReport/LaunchValuedMovementsDetail.java
+++ client/Src/org/openXpertya/JasperReport/LaunchValuedMovementsDetail.java
@@ -2,6 +2,7 @@ package org.openXpertya.JasperReport;
 
 import org.openXpertya.JasperReport.DataSource.OXPJasperDataSource;
 import org.openXpertya.JasperReport.DataSource.ValuedMovementsDetailDataSource;
+import org.openXpertya.model.MProductLines;
 
 public class LaunchValuedMovementsDetail extends LaunchValuedMovements {
 
@@ -9,7 +10,24 @@ public class LaunchValuedMovementsDetail extends LaunchValuedMovements {
 	protected OXPJasperDataSource createReportDataSource() {
 		return new ValuedMovementsDetailDataSource(getCtx(), getOrgID(),
 				getDateFrom(), getDateTo(), getWarehouseID(),
-				getPriceListVersionID(), getChargeID(), get_TrxName());
+				getPriceListVersionID(), getChargeID(), getProductLineID(), 
+				get_TrxName());
+	}
+	
+	@Override
+	protected void loadReportParameters() throws Exception {
+		super.loadReportParameters();
+		// Línea de Artículo
+		if(getProductLineID() != null){
+			MProductLines productLine = new MProductLines(getCtx(),
+					getProductLineID(), get_TrxName());
+			addReportParameter("PRODUCT_LINE_VALUE", productLine.getValue());
+			addReportParameter("PRODUCT_LINE_NAME", productLine.getName());
+		}
+	}
+	
+	protected Integer getProductLineID(){
+		return (Integer)getParameterValue("M_Product_Lines_ID");
 	}
 
 }
