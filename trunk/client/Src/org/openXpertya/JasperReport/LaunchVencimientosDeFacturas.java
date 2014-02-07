345a33555e0c430b926e1dbfa3cc9ad3b95fb23c
mauro.hernandez@disytel.net <mauro.hernandez@disytel.net@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2014-02-07 13:47:03 +0000

    Informe de vencimientos. Se agregó la opción de subtotalizar por entidad comercial  y por fecha

--- trunk/client/Src/org/openXpertya/JasperReport/LaunchVencimientosDeFacturas.java
+++ trunk/client/Src/org/openXpertya/JasperReport/LaunchVencimientosDeFacturas.java
@@ -12,9 +12,9 @@ public class LaunchVencimientosDeFacturas extends JasperReportLaunch {
 	@Override
 	protected void loadReportParameters() throws Exception {
 		// Nombre de la organización
-		addReportParameter("ad_org_name",getOrgName());
+		addReportParameter("ad_org_name", getOrgName());
 		// Nombre de la entidad comercial
-		addReportParameter("bpartner",getBPartnerName());
+		addReportParameter("bpartner", getBPartnerName());
 		// Mostrar Subtotales
 		addReportParameter("subtotales", getSubtotales());
 		// Fecha Desde
@@ -25,95 +25,108 @@ public class LaunchVencimientosDeFacturas extends JasperReportLaunch {
 		addReportParameter("TRX_TYPE_NAME", getTrxTypeInfo());
 		// Filtro de fechas
 		addReportParameter("DATE_FILTER_NAME", getDateFilterInfo());
+		// Subtotales Por
+		addReportParameter("subtotales_por", getSubtotalesPor());
 	}
-	
+
 	/**
 	 * @return Nombre de la organización o todos
 	 */
-	private String getOrgName(){
+	private String getOrgName() {
 		Integer orgID = getOrgID();
 		return Util.isEmpty(orgID, true) ? "Todos" : JasperReportsUtil
 				.getOrgName(getCtx(), orgID);
 	}
-	
+
 	/**
 	 * @return Nombre de la entidad comercial o todos
 	 */
-	private String getBPartnerName(){
+	private String getBPartnerName() {
 		Integer bPartnerID = getBPartnerID();
 		return Util.isEmpty(bPartnerID, true) ? "Todos" : JasperReportsUtil
 				.getBPartnerName(getCtx(), bPartnerID, get_TrxName());
 	}
-	
+
 	/**
 	 * @return fecha desde
 	 */
-	private Timestamp getDateFrom(){
-		return (Timestamp)getParameterValue("DateInvoiced");
+	private Timestamp getDateFrom() {
+		return (Timestamp) getParameterValue("DateInvoiced");
 	}
-	
+
 	/**
 	 * @return fecha hasta
 	 */
-	private Timestamp getDateTo(){
-		return (Timestamp)getParameterValue("DateInvoiced_TO");
+	private Timestamp getDateTo() {
+		return (Timestamp) getParameterValue("DateInvoiced_TO");
 	}
-	
+
 	/**
 	 * @return id de la organización
 	 */
-	private Integer getOrgID(){
-		return (Integer)getParameterValue("AD_Org_ID");
+	private Integer getOrgID() {
+		return (Integer) getParameterValue("AD_Org_ID");
 	}
-	
+
 	/**
 	 * @return id de la entidad comercial
 	 */
-	private Integer getBPartnerID(){
-		return (Integer)getParameterValue("C_BPartner_ID");
+	private Integer getBPartnerID() {
+		return (Integer) getParameterValue("C_BPartner_ID");
 	}
-	
+
 	/**
 	 * @return filtro de fechas
 	 */
-	private String getDateFilter(){
+	private String getDateFilter() {
 		return (String) getParameterValue("DateFilter");
 	}
-	
+
 	/**
 	 * @return tipo de transacción
 	 */
-	private String getTrxType(){
+	private String getTrxType() {
 		return (String) getParameterValue("TrxType");
 	}
-	
+
 	/**
 	 * @return Info del tipo de transacción
 	 */
-	private String getTrxTypeInfo(){
+	private String getTrxTypeInfo() {
 		return getParameterInfo("TrxType");
 	}
 
 	/**
 	 * @return Info del filtro de fechas
 	 */
-	private String getDateFilterInfo(){
+	private String getDateFilterInfo() {
 		return getParameterInfo("DateFilter");
 	}
-	
+
+	/**
+	 * @return por lo cual se requiere subtotal
+	 */
+	private String getSubtotalesPor() {
+		if (new String("Fecha")
+				.equals((String) getParameterValue("subtotales_por")))
+			return "Fecha";
+		else
+			return "Entidad Comercial";
+	}
+
 	/**
 	 * @return true si se deben agregar los subtotales por EC, false caso
 	 *         contrario
 	 */
-	private Boolean getSubtotales(){
-		return ((String)getParameterValue("subtotales")).equals("Y");
+	private Boolean getSubtotales() {
+		return ((String) getParameterValue("subtotales")).equals("Y");
 	}
-	
+
 	@Override
 	protected OXPJasperDataSource createReportDataSource() {
 		return new VencimientoDeFacturasDataSource(getCtx(), getDateFrom(),
 				getDateTo(), getOrgID(), getBPartnerID(), getDateFilter(),
-				getTrxType(), get_TrxName());
+				getTrxType(), get_TrxName(), getSubtotalesPor());
 	}
 
 }
