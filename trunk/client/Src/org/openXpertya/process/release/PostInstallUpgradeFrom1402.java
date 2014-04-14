24fcfc823fb24204611c13a9710ec54130ea8034
juan.manuel.castro@disytel.net <juan.manuel.castro@disytel.net@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2014-04-14 03:33:36 +0000

    Incorporación de reportes al postinstall.

--- trunk/client/Src/org/openXpertya/process/release/PostInstallUpgradeFrom1402.java
+++ trunk/client/Src/org/openXpertya/process/release/PostInstallUpgradeFrom1402.java
@@ -1,6 +1,7 @@
 package org.openXpertya.process.release;
 
 import org.openXpertya.JasperReport.MJasperReport;
+import org.openXpertya.model.MProcess;
 import org.openXpertya.process.PluginPostInstallProcess;
 import org.openXpertya.utils.JarHelper;
 
@@ -9,7 +10,30 @@ public class PostInstallUpgradeFrom1402 extends PluginPostInstallProcess {
 	/** UID del informe de Movimientos de artículo detallado */
 	protected final static String PRODUCT_MOVEMENTS_DETAILED_JASPER_REPORT_UID = "CORE-AD_JasperReport-1010092";
 	protected final static String PRODUCT_MOVEMENTS_DETAILED_JASPER_REPORT_FILENAME = "ProductMovementsWithStockBalance.jasper";
-
+	
+	/** UID del Informe de Libro de IVA */
+	protected final static String LIBRO_IVA_REPORT_JASPER_REPORT_UID = "LIVA2CORE-AD_JasperReport-1010047-20121031201418";
+	protected final static String LIBRO_IVA_REPORT_JASPER_REPORT_FILENAME = "InformeLibroIVA.jasper";
+	
+	/** UID del Subreporte de impuestos del Informe de Libro de IVA */
+	protected final static String LIBRO_IVA_TAX_SUBREPORT_JASPER_REPORT_UID = "LIVA2CORE-AD_JasperReport-1010053-20121031201630";
+	protected final static String LIBRO_IVA_TAX_SUBREPORT_JASPER_REPORT_FILENAME = "SubReport_TaxInformeLibroIva.jasper";
+	
+	/** UID del Libro de Mayor */
+	protected final static String DIARIO_DEL_MAYOR_JASPER_REPORT_UID = "CORE-AD_JasperReport-1000007";
+	protected final static String DIARIO_DEL_MAYOR_JASPER_REPORT_FILENAME = "DiarioMayor.jasper";
+	
+	/** UID del Informe de Ventas por Provincia */
+	protected final static String REPORT_SALES_BY_PROVINCE_REPORT_UID = "CORE-AD_Process-1010380";
+	protected final static String REPORT_SALES_BY_PROVINCE_REPORT_FILENAME = "report_sales_by_province.jrxml";
+	
+	/** UID del Informe de Libro Diario */
+	protected final static String REPORT_JOURNAL_BOOK_REPORT_UID = "CORE-AD_Process-1010379";
+	protected final static String REPORT_JOURNAL_BOOK_REPORT_FILENAME = "LibroDiario.jrxml";
+	
+	/** UID del Informe de inventario de bienes de uso */
+	protected final static String INVENTORY_ASSETS_JASPER_REPORT_UID = "CORE-AD_JasperReport-1010046";
+	protected final static String INVENTORY_ASSETS_JASPER_REPORT_FILENAME = "InventoryAssetsReport.jasper";
 	
 	protected String doIt() throws Exception {
 		super.doIt();
@@ -33,6 +57,72 @@ public class PostInstallUpgradeFrom1402 extends PluginPostInstallProcess {
 										jarFileURL,
 										getBinaryFileURL(PRODUCT_MOVEMENTS_DETAILED_JASPER_REPORT_FILENAME)));
 		
+		// Informe de Libro de IVA
+		MJasperReport
+			.updateBinaryData(
+					get_TrxName(),
+					getCtx(),
+					LIBRO_IVA_REPORT_JASPER_REPORT_UID,
+					JarHelper
+							.readBinaryFromJar(
+									jarFileURL,
+									getBinaryFileURL(LIBRO_IVA_REPORT_JASPER_REPORT_FILENAME)));
+		
+		// Subreporte de impuestos del Informe de Libro de IVA
+		MJasperReport
+			.updateBinaryData(
+					get_TrxName(),
+					getCtx(),
+					LIBRO_IVA_TAX_SUBREPORT_JASPER_REPORT_UID,
+					JarHelper
+							.readBinaryFromJar(
+									jarFileURL,
+									getBinaryFileURL(LIBRO_IVA_TAX_SUBREPORT_JASPER_REPORT_FILENAME)));
+		
+		// Informe de Libro del Mayor
+		MJasperReport
+			.updateBinaryData(
+					get_TrxName(),
+					getCtx(),
+					DIARIO_DEL_MAYOR_JASPER_REPORT_UID,
+					JarHelper
+							.readBinaryFromJar(
+									jarFileURL,
+									getBinaryFileURL(DIARIO_DEL_MAYOR_JASPER_REPORT_FILENAME)));
+				
+		// Informe de Ventas por Provincia
+		MProcess.addAttachment(
+				get_TrxName(),
+				getCtx(),
+				REPORT_SALES_BY_PROVINCE_REPORT_UID,
+				REPORT_SALES_BY_PROVINCE_REPORT_FILENAME,
+				JarHelper
+						.readBinaryFromJar(
+								jarFileURL,
+								getBinaryFileURL(REPORT_SALES_BY_PROVINCE_REPORT_FILENAME)));
+		
+		// Informe de Libro Diario
+		MProcess.addAttachment(
+				get_TrxName(),
+				getCtx(),
+				REPORT_JOURNAL_BOOK_REPORT_UID,
+				REPORT_JOURNAL_BOOK_REPORT_FILENAME,
+				JarHelper
+						.readBinaryFromJar(
+								jarFileURL,
+								getBinaryFileURL(REPORT_JOURNAL_BOOK_REPORT_FILENAME)));
+		
+		// Informe de inventario de bienes de uso
+		MJasperReport
+		.updateBinaryData(
+				get_TrxName(),
+				getCtx(),
+				INVENTORY_ASSETS_JASPER_REPORT_UID,
+				JarHelper
+						.readBinaryFromJar(
+								jarFileURL,
+								getBinaryFileURL(INVENTORY_ASSETS_JASPER_REPORT_FILENAME)));
+				
 		return " ";
 	}
 
