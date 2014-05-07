d9e92a7c6aef70ac1aa59110ff3e142c100bac57
matias.cap <matias.cap@disytel.net>
2014-05-07 04:42:29 +0000

    Mejoras de performance al completar inventario. No se registran transaction con movimiento en 0. Modificación del reporte de detalle de movimientos por artículo para tener en cuenta esos inventarios no transactionados

--- data/core/upgrade_from_14.02/preinstall_from_14.02.sql
+++ data/core/upgrade_from_14.02/preinstall_from_14.02.sql
@@ -1449,3 +1449,98 @@ update ad_system set dummy = (SELECT addcolumnifnotexists('C_Invoice','C_Region_
 update c_retschema_config
 set padrontype = 'B', orden = 1
 where ad_client_id = 1010016 and name = 'P';
+
+--20140507-0130 Nueva vista para incorporar los inventarios que no se registran en transaction por cantidad 0
+CREATE OR REPLACE VIEW v_product_movements_detailed AS 
+ SELECT t.movement_table, t.ad_client_id, t.ad_org_id, t.m_locator_id, w.m_warehouse_id, w.value AS warehouse_value, w.name AS warehouse_name, t.receiptvalue, t.movementdate, t.doctypename, t.documentno, t.docstatus, t.m_product_id, t.product_value, t.product_name, t.qty, t.c_invoice_id, i.documentno AS invoice_documentno
+   FROM (        (        (        (        (         SELECT 'M_InOut' AS movement_table, t.ad_client_id, t.ad_org_id, t.m_locator_id, 
+                                                        CASE dt.signo_issotrx
+                                                            WHEN 1 THEN 'Y'::text
+                                                            ELSE 'N'::text
+                                                        END AS receiptvalue, t.movementdate, dt.name AS doctypename, io.documentno, io.docstatus, p.m_product_id, p.value AS product_value, p.name AS product_name, abs(t.movementqty) AS qty, ( SELECT i.c_invoice_id
+                                                           FROM c_order o
+                                                      JOIN c_invoice i ON i.c_order_id = o.c_order_id
+                                                     WHERE o.c_order_id = io.c_order_id
+                                                    LIMIT 1) AS c_invoice_id
+                                                   FROM m_transaction t
+                                              JOIN m_inoutline iol ON iol.m_inoutline_id = t.m_inoutline_id
+                                         JOIN m_product p ON p.m_product_id = t.m_product_id
+                                    JOIN m_inout io ON io.m_inout_id = iol.m_inout_id
+                               JOIN c_doctype dt ON dt.c_doctype_id = io.c_doctype_id
+                                        UNION ALL 
+                                                 SELECT 'M_Movement' AS movement_table, t.ad_client_id, t.ad_org_id, t.m_locator_id, 
+                                                        CASE abs(t.movementqty)
+                                                            WHEN t.movementqty THEN 'Y'::text
+                                                            ELSE 'N'::text
+                                                        END AS receiptvalue, t.movementdate, dt.name AS doctypename, m.documentno, m.docstatus, p.m_product_id, p.value AS product_value, p.name AS product_name, abs(t.movementqty) AS qty, NULL::unknown AS c_invoice_id
+                                                   FROM m_transaction t
+                                              JOIN m_movementline ml ON ml.m_movementline_id = t.m_movementline_id
+                                         JOIN m_product p ON p.m_product_id = t.m_product_id
+                                    JOIN m_movement m ON m.m_movement_id = ml.m_movement_id
+                               JOIN c_doctype dt ON dt.c_doctype_id = m.c_doctype_id)
+                                UNION ALL 
+                                         SELECT 'M_Inventory' AS movement_table, t.ad_client_id, t.ad_org_id, t.m_locator_id, 
+                                                CASE abs(t.movementqty)
+                                                    WHEN t.movementqty THEN 'Y'::text
+                                                    ELSE 'N'::text
+                                                END AS receiptvalue, t.movementdate, dt.name AS doctypename, i.documentno, i.docstatus, p.m_product_id, p.value AS product_value, p.name AS product_name, abs(t.movementqty) AS qty, NULL::unknown AS c_invoice_id
+                                           FROM m_transaction t
+                                      JOIN m_inventoryline il ON il.m_inventoryline_id = t.m_inventoryline_id
+                                 JOIN m_product p ON p.m_product_id = t.m_product_id
+                            JOIN m_inventory i ON i.m_inventory_id = il.m_inventory_id
+                       JOIN c_doctype dt ON dt.c_doctype_id = i.c_doctype_id
+                  LEFT JOIN m_transfer tr ON tr.m_inventory_id = i.m_inventory_id
+             LEFT JOIN m_splitting sp ON sp.m_inventory_id = i.m_inventory_id
+        LEFT JOIN m_productchange pc ON pc.m_inventory_id = i.m_inventory_id
+       WHERE tr.m_transfer_id IS NULL AND sp.m_splitting_id IS NULL AND pc.m_productchange_id IS NULL
+       UNION ALL
+SELECT 'M_Inventory' AS movement_table, i.ad_client_id, i.ad_org_id, il.m_locator_id, 
+                                                CASE WHEN (il.qtycount - il.qtybook) >= 0 THEN 'Y'::text
+                                                    ELSE 'N'::text
+                                                END AS receiptvalue, i.movementdate, dt.name AS doctypename, i.documentno, i.docstatus, p.m_product_id, p.value AS product_value, p.name AS product_name, abs(il.qtycount - il.qtybook) AS qty, NULL::unknown AS c_invoice_id
+                                           FROM m_inventory i
+                                      JOIN m_inventoryline il ON i.m_inventory_id = il.m_inventory_id
+                                 JOIN m_product p ON p.m_product_id = il.m_product_id
+                       JOIN c_doctype dt ON dt.c_doctype_id = i.c_doctype_id
+                  LEFT JOIN m_transfer tr ON tr.m_inventory_id = i.m_inventory_id
+             LEFT JOIN m_splitting sp ON sp.m_inventory_id = i.m_inventory_id
+        LEFT JOIN m_productchange pc ON pc.m_inventory_id = i.m_inventory_id
+       WHERE NOT EXISTS (SELECT m_transaction_id FROM m_transaction t WHERE il.m_inventoryline_id = t.m_inventoryline_id) AND tr.m_transfer_id IS NULL AND sp.m_splitting_id IS NULL AND pc.m_productchange_id IS NULL)
+                        UNION ALL 
+                                 SELECT 'M_Transfer' AS movement_table, t.ad_client_id, t.ad_org_id, t.m_locator_id, 
+                                        CASE abs(t.movementqty)
+                                            WHEN t.movementqty THEN 'Y'::text
+                                            ELSE 'N'::text
+                                        END AS receiptvalue, t.movementdate, tr.transfertype AS doctypename, tr.documentno, tr.docstatus, p.m_product_id, p.value AS product_value, p.name AS product_name, abs(t.movementqty) AS qty, NULL::unknown AS c_invoice_id
+                                   FROM m_transaction t
+                              JOIN m_inventoryline il ON il.m_inventoryline_id = t.m_inventoryline_id
+                         JOIN m_product p ON p.m_product_id = t.m_product_id
+                    JOIN m_inventory i ON i.m_inventory_id = il.m_inventory_id
+               JOIN m_transfer tr ON tr.m_inventory_id = i.m_inventory_id)
+                UNION ALL 
+                         SELECT 'M_Splitting' AS movement_table, t.ad_client_id, t.ad_org_id, t.m_locator_id, 
+                                CASE abs(t.movementqty)
+                                    WHEN t.movementqty THEN 'Y'::text
+                                    ELSE 'N'::text
+                                END AS receiptvalue, t.movementdate, 'M_Splitting_ID' AS doctypename, sp.documentno, sp.docstatus, p.m_product_id, p.value AS product_value, p.name AS product_name, abs(t.movementqty) AS qty, NULL::unknown AS c_invoice_id
+                           FROM m_transaction t
+                      JOIN m_inventoryline il ON il.m_inventoryline_id = t.m_inventoryline_id
+                 JOIN m_product p ON p.m_product_id = t.m_product_id
+            JOIN m_inventory i ON i.m_inventory_id = il.m_inventory_id
+       JOIN m_splitting sp ON sp.m_inventory_id = i.m_inventory_id)
+        UNION ALL 
+                 SELECT 'M_ProductChange' AS movement_table, t.ad_client_id, t.ad_org_id, t.m_locator_id, 
+                        CASE abs(t.movementqty)
+                            WHEN t.movementqty THEN 'Y'::text
+                            ELSE 'N'::text
+                        END AS receiptvalue, t.movementdate, 'M_ProductChange_ID' AS doctypename, pc.documentno, pc.docstatus, p.m_product_id, p.value AS product_value, p.name AS product_name, abs(t.movementqty) AS qty, NULL::unknown AS c_invoice_id
+                   FROM m_transaction t
+              JOIN m_inventoryline il ON il.m_inventoryline_id = t.m_inventoryline_id
+         JOIN m_product p ON p.m_product_id = t.m_product_id
+    JOIN m_inventory i ON i.m_inventory_id = il.m_inventory_id
+   JOIN m_productchange pc ON pc.m_inventory_id = i.m_inventory_id) t
+   JOIN m_locator l ON l.m_locator_id = t.m_locator_id
+   JOIN m_warehouse w ON w.m_warehouse_id = l.m_warehouse_id
+   LEFT JOIN c_invoice i ON i.c_invoice_id = t.c_invoice_id;
+
+ALTER TABLE v_product_movements_detailed OWNER TO libertya;
