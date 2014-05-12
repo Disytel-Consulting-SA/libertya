a3069351419823a93fbdf780cb4e5522ce6117ff
matias.cap@disytel.net <matias.cap@disytel.net@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2014-05-12 19:36:35 +0000

    Cambio de insert de columna por set dummy

--- trunk/data/core/upgrade_from_14.02/preinstall_from_14.02.sql
+++ trunk/data/core/upgrade_from_14.02/preinstall_from_14.02.sql
@@ -1552,4 +1552,4 @@ update ad_system set dummy = (SELECT addcolumnifnotexists('C_DocType','validates
 update ad_system set dummy = (SELECT addcolumnifnotexists('AD_ClientInfo','UniqueCuit', 'character(1) NOT NULL DEFAULT ''N''::bpchar'));
 
 --20140512-1030 Incorporación de nueva parametrización en proveedores para configurar el monto mínimo de compra (en ordenes de compra)
-ALTER TABLE c_bpartner ADD COLUMN minimumpurchasedamt numeric(22,9) NOT NULL DEFAULT 0;
+update ad_system set dummy = (SELECT addcolumnifnotexists('C_BPartner','minimumpurchasedamt', 'numeric(22,9) NOT NULL DEFAULT 0'));
