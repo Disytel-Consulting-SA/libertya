6560cadf374a1908b5e0e2b9b02703208d419da8
matias.cap@disytel.net <matias.cap@disytel.net@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2014-10-28 15:06:50 +0000

    Modificaciones que permiten extensión para subclases

--- trunk/client/Src/org/openXpertya/JasperReport/LaunchDeclaracionValoresXOrg.java
+++ trunk/client/Src/org/openXpertya/JasperReport/LaunchDeclaracionValoresXOrg.java
@@ -24,40 +24,23 @@ public class LaunchDeclaracionValoresXOrg extends LaunchDeclaracionValores {
 	@Override
 	protected void addSubreports() throws Exception{
 		// Cupon
-		DeclaracionValoresCuponDataSource cuponDS = new DeclaracionValoresCuponDataSource(
-				getCtx(), getValoresDTO(), getSelect(), getGroupBy(),
-				getOrderBy(), get_TrxName());
+		DeclaracionValoresCuponDataSource cuponDS = getCuponDS();
 		// Venta
 		// La venta pasada como parámetro se debe sacar del resumen de ventas
-		ResumenVentasDocTypeBaseKeyDataSource ventaDS = new ResumenVentasDocTypeBaseKeyDataSource(
-				get_TrxName(), getCtx(), getValoresDTO().getOrgID(),
-				getValoresDTO().getDateFrom(), getValoresDTO().getDateTo(),
-				null, getValoresDTO().getUserID(), false, false);
+		ResumenVentasDocTypeBaseKeyDataSource ventaDS = getVentaDS();
 		// Efectivo
-		DeclaracionValoresCashDataSource cashDS = new DeclaracionValoresCashDataSource(
-				getCtx(), getValoresDTO(), getSelect(), getGroupBy(),
-				getOrderBy(), get_TrxName());
+		DeclaracionValoresCashDataSource cashDS = getCashDS();
 		// Cheque
-		DeclaracionValoresCheckDataSource checkDS = new DeclaracionValoresCheckDataSource(
-				getCtx(), getValoresDTO(), getSelect(), getGroupBy(),
-				getOrderBy(), get_TrxName());
+		DeclaracionValoresCheckDataSource checkDS = getCheckDS();
 		// Transferencia
-		DeclaracionValoresTransferDataSource transferDS = new DeclaracionValoresTransferDataSource(
-				getCtx(), getValoresDTO(), getSelect(), getGroupBy(),
-				getOrderBy(), get_TrxName());
+		DeclaracionValoresTransferDataSource transferDS = getTransferDS();
 		// NC
-		DeclaracionValoresCreditNoteDataSource creditNoteDS = new DeclaracionValoresCreditNoteDataSource(
-				getCtx(), getValoresDTO(), getSelect(), getGroupBy(),
-				getOrderBy(), get_TrxName());
+		DeclaracionValoresCreditNoteDataSource creditNoteDS = getCreditNoteDS();
 		// Cta Cte
-		DeclaracionValoresCuentaCorrienteDataSource ccDS = new DeclaracionValoresCuentaCorrienteDataSource(
-				getCtx(), getValoresDTO(), getSelect(), getGroupBy(),
-				getOrderBy(), get_TrxName());
+		DeclaracionValoresCuentaCorrienteDataSource ccDS = getCurrentAccountDS();
 		// Cobranza
-		ResumenVentasCurrentAccountPaymentsDataSource capDS = new ResumenVentasCurrentAccountPaymentsDataSource(
-				get_TrxName(), getCtx(), getValoresDTO().getOrgID(),
-				getValoresDTO().getDateFrom(), getValoresDTO().getDateTo(),
-				null, getValoresDTO().getUserID(), false, false);
+		ResumenVentasCurrentAccountPaymentsDataSource capDS = getCurrentAccountPaymentsDS();
+		// Agregar subreportes
 		addReportParameter("TOTAL_CUPON", cuponDS != null?cuponDS.getTotalAmt():null);
 		addReportParameter("TOTAL_VENTA", ventaDS != null?ventaDS.getTotalAmt():null);
 		addReportParameter("TOTAL_EFECTIVO", cashDS != null?cashDS.getTotalAmt():null);
@@ -66,5 +49,56 @@ public class LaunchDeclaracionValoresXOrg extends LaunchDeclaracionValores {
 		addReportParameter("TOTAL_NC", creditNoteDS != null?creditNoteDS.getTotalAmt():null);
 		addReportParameter("TOTAL_CC", ccDS != null?ccDS.getTotalAmt():null);
 		addReportParameter("TOTAL_COBRANZA", capDS != null?capDS.getTotalAmt():null);
+	}
+	
+	
+	protected DeclaracionValoresCuponDataSource getCuponDS(){
+		return new DeclaracionValoresCuponDataSource(
+				getCtx(), getValoresDTO(), getSelect(), getGroupBy(),
+				getOrderBy(), get_TrxName());
+	}
+	
+	protected ResumenVentasDocTypeBaseKeyDataSource getVentaDS(){
+		return new ResumenVentasDocTypeBaseKeyDataSource(
+				get_TrxName(), getCtx(), getValoresDTO().getOrgID(),
+				getValoresDTO().getDateFrom(), getValoresDTO().getDateTo(),
+				null, getValoresDTO().getUserID(), false, false);
+	}
+	
+	protected DeclaracionValoresCashDataSource getCashDS(){
+		return new DeclaracionValoresCashDataSource(
+				getCtx(), getValoresDTO(), getSelect(), getGroupBy(),
+				getOrderBy(), get_TrxName());
+	}
+	
+	protected DeclaracionValoresCheckDataSource getCheckDS(){
+		return new DeclaracionValoresCheckDataSource(
+				getCtx(), getValoresDTO(), getSelect(), getGroupBy(),
+				getOrderBy(), get_TrxName());
+	}
+	
+	protected DeclaracionValoresTransferDataSource getTransferDS(){
+		return new DeclaracionValoresTransferDataSource(
+				getCtx(), getValoresDTO(), getSelect(), getGroupBy(),
+				getOrderBy(), get_TrxName());
+	}
+	
+	protected DeclaracionValoresCreditNoteDataSource getCreditNoteDS(){
+		return new DeclaracionValoresCreditNoteDataSource(
+				getCtx(), getValoresDTO(), getSelect(), getGroupBy(),
+				getOrderBy(), get_TrxName());
+	}
+	
+	protected DeclaracionValoresCuentaCorrienteDataSource getCurrentAccountDS(){
+		return new DeclaracionValoresCuentaCorrienteDataSource(
+				getCtx(), getValoresDTO(), getSelect(), getGroupBy(),
+				getOrderBy(), get_TrxName());
+	}
+	
+	protected ResumenVentasCurrentAccountPaymentsDataSource getCurrentAccountPaymentsDS(){
+		return new ResumenVentasCurrentAccountPaymentsDataSource(
+				get_TrxName(), getCtx(), getValoresDTO().getOrgID(),
+				getValoresDTO().getDateFrom(), getValoresDTO().getDateTo(),
+				null, getValoresDTO().getUserID(), false, false);
 	}
 }
