acb54fc0bf47773d9d3dd9d86334af78b7e03e95
federico.cristina@disytel.net <federico.cristina@disytel.net@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2014-12-03 22:39:19 +0000

    Replicacion: Forzar la replicacion en caso de ser necesario para el filtro BPartnerReplicationFilter

--- trunk/base/src/org/openXpertya/model/MTableReplication.java
+++ trunk/base/src/org/openXpertya/model/MTableReplication.java
@@ -13,8 +13,10 @@ import org.openXpertya.util.DB;
 
 public class MTableReplication extends X_AD_TableReplication {
 
-	/** Cache de replicationArray según AD_TableReplication */
+	/** Cache de replicationArray según AD_TableReplication por ID de tabla */
 	protected static HashMap<Integer, String> replicationArrayByTable = null;
+	/** Cache de replicationArray según AD_TableReplication por nombre de tabla (en minuscula) */
+	protected static HashMap<String, String> replicationArrayByTableName = null;
 	
 	/** Logger */
     private static CLogger	s_log	= CLogger.getCLogger(MChangeLog.class);
@@ -32,8 +34,7 @@ public class MTableReplication extends X_AD_TableReplication {
 	}
 	
 	/**
-	 * Dadas una tabla y una sucursal en especial, 
-	 * retorna el replicationArray correspondiente
+	 * Dado el ID de una tabla retorna el replicationArray correspondiente
 	 */
 	public static String getReplicationArray(int tableID, String trxName)
 	{
@@ -44,19 +45,33 @@ public class MTableReplication extends X_AD_TableReplication {
 	}
 	
 	/**
+	 * Dado el nombre de una tabla retorna el replicationArray correspondiente
+	 */
+	public static String getReplicationArray(String tableName, String trxName)
+	{
+		if (replicationArrayByTableName == null)
+			loadReplicationArrayByTable(trxName);
+
+		return replicationArrayByTableName.get(tableName.toLowerCase());
+	}
+	
+	/**
 	 * Carga inicial en variable replicationArrayByTable usada para cache
 	 */
 	protected static void loadReplicationArrayByTable(String trxName)
 	{
 		replicationArrayByTable = new HashMap<Integer, String>();
+		replicationArrayByTableName = new HashMap<String, String>();
 		try
 		{
-			// Obtener la nomina completa por tabla. SE SUPONE QUE LAS ENTRADAS EXISTENTES SON DEL HOST ACTUAL (SUCURSAL)
-			PreparedStatement pstmt = DB.prepareStatement( " SELECT AD_Table_ID, replicationArray FROM AD_TableReplication ", trxName);
+			// Obtener la nomina completa por tabla
+			PreparedStatement pstmt = DB.prepareStatement( " SELECT tr.AD_Table_ID, tr.replicationArray, lower(t.tablename) FROM AD_TableReplication tr INNER JOIN AD_Table t ON t.AD_Table_ID = tr.AD_Table_ID ", trxName);
 			ResultSet rs = pstmt.executeQuery();
 		
-			while (rs.next())
+			while (rs.next()) {
 				replicationArrayByTable.put(rs.getInt(1), rs.getString(2));
+				replicationArrayByTableName.put(rs.getString(3), rs.getString(2));
+			}
 		}
 		catch (Exception e)
 		{
