07a9aa6cb07a702f8e23dcfccb4b149063b8534e
federico.cristina <118191354+fcristina77@users.noreply.github.com>
2015-03-02 16:35:32 +0000

    Correcciones y mejoras sobre r1210 (cont.)

--- data/core/upgrade_from_14.02/changeset/trxTimeOut.sh
+++ data/core/upgrade_from_14.02/changeset/trxTimeOut.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LY_HOME=/ServidorOXP
+
 PG_USR=libertya
 PG_DBN=libertya_prod
 PG_HST=localhost
@@ -9,20 +9,20 @@ PG_KILL_MAIL=daemon_target_mail.txt
 PG_KILL_LOG=$HOME/daemon_trxtimeout_$(date +\%Y\%m\%d).log
 
 # Verificar si hay un caso para matar
-$PG_BIN/psql -h $PG_HST -U $PG_USR -t -d $PG_DBN -f $LY_HOME/utils/trxTimeOut.sql -v p_dbname="'$PG_DBN'" -v p_timeout="'1 minute'" > $PG_KILL_PID;
+sudo -u postgres $PG_BIN/psql -h $PG_HST -U $PG_USR -t -d $PG_DBN -f trxTimeOut.sql -v p_dbname="'$PG_DBN'" -v p_timeout="'1 minute'" > $PG_KILL_PID;
 TARGET_LOCKED=`cat $PG_KILL_PID`;
 
 # Tenemos un pid que matar?
 if [ $TARGET_LOCKED ]; then
 	echo "$(date +\%H:\%M:\%S) KILLING PID $TARGET_LOCKED" >> $PG_KILL_LOG;
 	echo "====================================================ACTIVITY=======================================================" >> $PG_KILL_LOG;
-	$PG_BIN/psql -h $PG_HST -U $PG_USR -t -d $PG_DBN -c "select * from pg_stat_activity" >> $PG_KILL_LOG;
+	sudo -u postgres $PG_BIN/psql -h $PG_HST -U $PG_USR -t -d $PG_DBN -c "select * from pg_stat_activity" >> $PG_KILL_LOG;
 	echo "======================================================LOCKS========================================================" >> $PG_KILL_LOG;
-	$PG_BIN/psql -h $PG_HST -U $PG_USR -t -d $PG_DBN -c "select * from pg_locks" >> $PG_KILL_LOG;
+	sudo -u postgres $PG_BIN/psql -h $PG_HST -U $PG_USR -t -d $PG_DBN -c "select * from pg_locks" >> $PG_KILL_LOG;
 	echo "===================================================================================================================" >> $PG_KILL_LOG;
 	kill $TARGET_LOCKED;
 
-	$PG_BIN/psql -h $PG_HST -U $PG_USR -t -d $PG_DBN -c "select value from ad_preference where attribute ilike 'ReplicationAdmin'" > $PG_KILL_MAIL;
+	sudo -u postgres $PG_BIN/psql -h $PG_HST -U $PG_USR -t -d $PG_DBN -c "select value from ad_preference where attribute ilike 'ReplicationAdmin'" -v p_dbname="'$PG_DBN'" > $PG_KILL_MAIL;
 	TARGET_MAIL=`cat $PG_KILL_MAIL`;
 	mail -s "IDLE IN TRANSACTION KILLED!" $TARGET_MAIL < $PG_KILL_LOG;
 else
