43ca40dab5eb297a39c2b0e746f9c575a63b3ae8
matias.cap <matias.cap@disytel.net>
2015-03-17 14:04:23 +0000

    Árbol de directorios y archivos de bitácora del nuevo componente

---
+++ data/core/upgrade_from_15.03/preinstall_from_15.03.sql
@@ -0,0 +1,7 @@
+-- ========================================================================================
+-- PREINSTALL FROM 15.03
+-- ========================================================================================
+-- Consideraciones importantes:
+--	1) NO hacer cambios en el archivo, realizar siempre APPENDs al final del mismo 
+-- 	2) Recordar realizar las adiciones con un comentario con formato YYYYMMDD-HHMM
+-- ========================================================================================
