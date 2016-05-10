2903954a001baec33f9d00400190d98566d49759
fcristina <118191354+fcristina77@users.noreply.github.com>
2016-05-10 14:54:07 +0000

    Estructuras para cambios en bbdd y de archivos

---
+++ data/core/upgrade_from_16.04/preinstall_from_16.04.sql
@@ -0,0 +1,8 @@
+-- ========================================================================================
+-- PREINSTALL FROM 16.04
+-- ========================================================================================
+-- Consideraciones importantes:
+--	1) NO hacer cambios en el archivo, realizar siempre APPENDs al final del mismo 
+-- 	2) Recordar realizar las adiciones con un comentario con formato YYYYMMDD-HHMM
+-- ========================================================================================
+
