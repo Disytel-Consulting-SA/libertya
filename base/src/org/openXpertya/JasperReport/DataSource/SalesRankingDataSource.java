6daa9fc06b880cad5c5c9527c216404d42ff7486
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2016-10-12 11:16:05 +0000

    Mejoras de performance al reporte Ranking de Ventas

--- base/src/org/openXpertya/JasperReport/DataSource/SalesRankingDataSource.java
+++ base/src/org/openXpertya/JasperReport/DataSource/SalesRankingDataSource.java
@@ -30,11 +30,13 @@ public class SalesRankingDataSource extends ProductLinesSalesDataSource {
 		StringBuffer sql = new StringBuffer("select value as product_value, name as product_name, " + getFacturadoSelectColumn() + " as facturado " +
 					 "from (select p.m_product_id, p.value, p.name, sum(((CASE i.istaxincluded WHEN 'Y' THEN linenetamt ELSE linetotalamt END) - il.documentdiscountamt) * dt.signo_issotrx) as total, sum(qtyinvoiced * dt.signo_issotrx) as qty " +
 					 "		from c_invoiceline as il " +
-					 "		inner join m_product as p on p.m_product_id = il.m_product_id " +
-					 "		left  join m_product_category as pc on pc.m_product_category_id = p.m_product_category_id " +
-					 "		left join m_product_gamas as pg on pg.m_product_gamas_id = pc.m_product_gamas_id " +
-					 "		left join m_product_lines as pl on pl.m_product_lines_id = pg.m_product_lines_id " +
-					 "		inner join c_invoice as i on i.c_invoice_id = il.c_invoice_id " +
+					 "		inner join m_product as p on p.m_product_id = il.m_product_id ");
+		if(!Util.isEmpty(getProductLinesID(), true)){
+			sql.append("		inner join m_product_category as pc on pc.m_product_category_id = p.m_product_category_id " +
+					 "		inner join m_product_gamas as pg on pg.m_product_gamas_id = pc.m_product_gamas_id " +
+					 "		inner join m_product_lines as pl on pl.m_product_lines_id = pg.m_product_lines_id ");
+		}
+		sql.append(  "		inner join c_invoice as i on i.c_invoice_id = il.c_invoice_id " +
 					 "		inner join c_doctype as dt on dt.c_doctype_id = i.c_doctypetarget_id ");
 		if(!Util.isEmpty(getVendorID(), true)){
 			sql.append(" inner join m_product_po as po on il.m_product_id = po.m_product_id ");
@@ -92,4 +94,8 @@ public class SalesRankingDataSource extends ProductLinesSalesDataSource {
 		this.vendorID = vendorID;
 	}
 	
+	@Override
+	protected boolean isQueryNoConvert(){
+		return true;
+	}
 }
