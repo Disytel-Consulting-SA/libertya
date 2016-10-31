be1d9a800805c6d3acb106eb9c46cb112dd77ce8
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2016-10-31 18:52:49 +0000

    Fix error de compilación en revision 1631

--- trunk/base/src/org/openXpertya/process/MassiveUpdateBPartnerCreditStatus.java
+++ trunk/base/src/org/openXpertya/process/MassiveUpdateBPartnerCreditStatus.java
@@ -1,7 +1,13 @@
 package org.openXpertya.process;
 
+import java.util.Properties;
 
 public class MassiveUpdateBPartnerCreditStatus extends MassiveUpdateBPartnerBalance {
+
+	public MassiveUpdateBPartnerCreditStatus(Properties ctx, String trxName) {
+		super(ctx, trxName);
+		// TODO Auto-generated constructor stub
+	}
 
 	protected boolean isUpdateBalance(){
 		return false;
