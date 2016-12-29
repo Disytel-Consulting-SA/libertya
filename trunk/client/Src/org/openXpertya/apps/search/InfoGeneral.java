86cf41474e2098fcd76a88895e138813d3bd5e20
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2016-12-29 20:06:28 +0000

    Fixes por listas con value integer

--- trunk/client/Src/org/openXpertya/apps/search/InfoGeneral.java
+++ trunk/client/Src/org/openXpertya/apps/search/InfoGeneral.java
@@ -361,9 +361,9 @@ public class InfoGeneral extends Info {
 
                 } else if( displayType == DisplayType.List ) {
                     if( Env.isBaseLanguage( Env.getCtx(),"AD_Ref_List" )) {
-                        colSql = new StringBuffer( "(SELECT l.Name FROM AD_Ref_List l WHERE l.AD_Reference_ID=" ).append( AD_Reference_Value_ID ).append( " AND l.Value=" ).append( columnName ).append( ") AS " ).append( columnName );
+                        colSql = new StringBuffer( "(SELECT l.Name FROM AD_Ref_List l WHERE l.AD_Reference_ID=" ).append( AD_Reference_Value_ID ).append( " AND l.Value='" ).append( columnName ).append( "') AS " ).append( columnName );
                     } else {
-                        colSql = new StringBuffer( "(SELECT t.Name FROM AD_Ref_List l, AD_Ref_List_Trl t " + "WHERE l.AD_Ref_List_ID=t.AD_Ref_List_ID AND l.AD_Reference_ID=" ).append( AD_Reference_Value_ID ).append( " AND l.Value=" ).append( columnName ).append( " AND t.AD_Language='" ).append( Env.getAD_Language( Env.getCtx())).append( "') AS " ).append( columnName );
+                        colSql = new StringBuffer( "(SELECT t.Name FROM AD_Ref_List l, AD_Ref_List_Trl t " + "WHERE l.AD_Ref_List_ID=t.AD_Ref_List_ID AND l.AD_Reference_ID=" ).append( AD_Reference_Value_ID ).append( " AND l.Value='" ).append( columnName ).append( "' AND t.AD_Language='" ).append( Env.getAD_Language( Env.getCtx())).append( "') AS " ).append( columnName );
                     }
 
                     colClass = String.class;
