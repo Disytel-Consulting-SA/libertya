3aa38846b86b7df3f35fa26907dbc65deb752c1b
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2017-01-18 15:43:20 +0000

    Propagar las excepciones para determinar el error real y no provocar NPE

--- trunk/base/src/org/openXpertya/amortization/AbstractAmortizationProcessorFactory.java
+++ trunk/base/src/org/openXpertya/amortization/AbstractAmortizationProcessorFactory.java
@@ -21,49 +21,39 @@ public abstract class AbstractAmortizationProcessorFactory {
 	 * @param trxName
 	 * @return
 	 */
-	private static AbstractAmortizationProcessor getProcessor(Properties ctx, Class<?>[] constructorParamTypes, Object[] constructorParamArgs, String trxName){
+	private static AbstractAmortizationProcessor getProcessor(Properties ctx, Class<?>[] constructorParamTypes, Object[] constructorParamArgs, String trxName) throws Exception{
 		AbstractAmortizationProcessor processor = null;
 		Integer amortizationMethodID = Env.getContextAsInt(ctx, "$M_Amortization_Method_ID");
-		try{
-			// Si no tiene método configurado, entonces error
-			if(Util.isEmpty(amortizationMethodID, true)){
-				throw new Exception(Msg.getMsg(ctx, "NotExistAmortizationMethodConfigured"));
-			}
-			MAmortizationMethod method = new MAmortizationMethod(ctx, amortizationMethodID, trxName);
-			MAmortizationProcessor amortizationProcessor = new MAmortizationProcessor(ctx,
-					method.getM_Amortization_Processor_ID(), trxName);
-			// Si no tiene nombre de clase configurada, entonces error
-			if(Util.isEmpty(amortizationProcessor.getClassname(), true)){
-				throw new Exception(Msg.getMsg(ctx, "AmortizationProcessorClassNameNotExist"));
-			}
-			processor = (AbstractAmortizationProcessor) ClassUtil.getInstance(
-					amortizationProcessor.getClassname(),
-					constructorParamTypes, constructorParamArgs);
-		} catch(ClassNotFoundException cnfe){
-			cnfe.printStackTrace();
-		} catch(IllegalAccessException iae){
-			iae.printStackTrace();
-		} catch(InstantiationException ie){
-			ie.printStackTrace();
-		} catch(Exception e){
-			e.printStackTrace();
+		// Si no tiene método configurado, entonces error
+		if(Util.isEmpty(amortizationMethodID, true)){
+			throw new Exception(Msg.getMsg(ctx, "NotExistAmortizationMethodConfigured"));
 		}
+		MAmortizationMethod method = new MAmortizationMethod(ctx, amortizationMethodID, trxName);
+		MAmortizationProcessor amortizationProcessor = new MAmortizationProcessor(ctx,
+				method.getM_Amortization_Processor_ID(), trxName);
+		// Si no tiene nombre de clase configurada, entonces error
+		if(Util.isEmpty(amortizationProcessor.getClassname(), true)){
+			throw new Exception(Msg.getMsg(ctx, "AmortizationProcessorClassNameNotExist"));
+		}
+		processor = (AbstractAmortizationProcessor) ClassUtil.getInstance(
+				amortizationProcessor.getClassname(),
+				constructorParamTypes, constructorParamArgs);
 		return processor;
 	}
 	
-	public static AbstractAmortizationProcessor getProcessor(Properties ctx, MAmortization amortization, String trxName){
+	public static AbstractAmortizationProcessor getProcessor(Properties ctx, MAmortization amortization, String trxName) throws Exception{
 		return getProcessor(ctx, new Class[] { Properties.class,
 				MAmortization.class, String.class }, new Object[] { ctx,
 				amortization, trxName }, trxName);
 	}
 	
-	public static AbstractAmortizationProcessor getProcessor(Properties ctx, Integer amortizationID, String trxName){
+	public static AbstractAmortizationProcessor getProcessor(Properties ctx, Integer amortizationID, String trxName) throws Exception{
 		return getProcessor(ctx, new Class[] { Properties.class,
 				Integer.class, String.class }, new Object[] { ctx,
 				amortizationID, trxName }, trxName);
 	}
 	
-	public static AbstractAmortizationProcessor getProcessor(Properties ctx, Timestamp date, String trxName){
+	public static AbstractAmortizationProcessor getProcessor(Properties ctx, Timestamp date, String trxName) throws Exception{
 		return getProcessor(ctx, new Class[] { Properties.class,
 				Timestamp.class, String.class }, new Object[] { ctx,
 				date, trxName }, trxName);
