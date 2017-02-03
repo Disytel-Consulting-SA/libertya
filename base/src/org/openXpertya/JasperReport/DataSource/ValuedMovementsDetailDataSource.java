d7240720d173bef64bc7d2fce2a63c5e38422494
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2017-02-03 17:55:21 +0000

    Mejoras de performance a los informes Movimientos por Artículo, Movimientos Valorizados y Detalle de Movimientos Valorizados.

--- base/src/org/openXpertya/JasperReport/DataSource/ValuedMovementsDetailDataSource.java
+++ base/src/org/openXpertya/JasperReport/DataSource/ValuedMovementsDetailDataSource.java
@@ -37,28 +37,17 @@ public class ValuedMovementsDetailDataSource extends ValuedMovementsDataSource {
 		StringBuffer sql = new StringBuffer("SELECT tablename, ad_org_id, orgvalue, orgname, doc_id, documentno, datetrx, c_charge_id, chargename, m_warehouse_id, warehousevalue, warehousename, m_warehouseto_id, warehousetovalue, warehousetoname, m_product_id, productvalue, productname, productlinesvalue, productlinesname, pricestd, sum(qty) as qty, sum(amt) as amt " +
 											" FROM (SELECT tablename, m.ad_org_id, orgvalue, orgname, doc_id, documentno, datetrx, c_charge_id, chargename, m_warehouse_id, warehousevalue, warehousename, m_warehouseto_id, warehousetovalue, warehousetoname, m.m_product_id, productvalue, productname, productlinesvalue, productlinesname, qty, coalesce(pp.pricestd,0) as pricestd, coalesce(pp.pricestd,0) * m.qty as amt " +
 											" FROM (SELECT tablename, ad_org_id, orgvalue, orgname, doc_id, documentno, datetrx, c_charge_id, chargename, m_warehouse_id, warehousevalue, warehousename, m_warehouseto_id, warehousetovalue, warehousetoname, m_product_id, productvalue, productname, productlinesvalue, productlinesname, qty " +
-											"		FROM v_product_movements " +
+											"		FROM v_product_movements_filtered( " + (getOrgID() != null ? getOrgID() : "-1") + ", "
+																						 + (getDateFrom() != null ? "'" + getDateFrom() + "'" : "null") + "::timestamp, "
+																						 + (getDateTo() != null ? "'" + getDateTo() + "'" : "null") + "::timestamp, "
+																						 + (getChargeID() != null ? getChargeID() : "-1") + ", "
+																						 + (getProductLinesID() != null ? getProductLinesID() : "-1") +") " +
 											"		WHERE docstatus IN ('CL','CO') " +
 											"				AND ad_client_id = ? " +
 											"				AND tablename <> 'M_InOut' " +
 											"				AND qty <> 0 ");
-		if(getOrgID() != null){
-			sql.append(" AND ad_org_id = ? ");
-		}
-		if(getDateFrom() != null){
-			sql.append(" AND datetrx >= ?::date ");
-		}
-		if(getDateTo() != null){
-			sql.append(" AND datetrx <= ?::date ");
-		}
 		if(getWarehouseID() != null){
 			sql.append(" AND ((tablename = 'M_Transfer' AND type = 'T' AND aditionaltype = 'I' AND m_warehouseto_id = ?) OR m_warehouse_id = ?) ");
-		}
-		if(getChargeID() != null){
-			sql.append(" AND c_charge_id = ? ");
-		}
-		if(getProductLinesID() != null){
-			sql.append(" AND m_product_lines_id = ? ");
 		}
 		sql.append(") as m ");
 		sql.append("LEFT JOIN m_productprice as pp on pp.m_product_id = m.m_product_id " +
@@ -72,24 +61,9 @@ public class ValuedMovementsDetailDataSource extends ValuedMovementsDataSource {
 	protected Object[] getParameters() {
 		List<Object> params = new ArrayList<Object>();
 		params.add(Env.getAD_Client_ID(getCtx()));
-		if(getOrgID() != null){
-			params.add(getOrgID());
-		}
-		if(getDateFrom() != null){
-			params.add(getDateFrom());
-		}
-		if(getDateTo() != null){
-			params.add(getDateTo());
-		}
 		if(getWarehouseID() != null){
 			params.add(getWarehouseID());
 			params.add(getWarehouseID());
-		}
-		if(getChargeID() != null){
-			params.add(getChargeID());
-		}
-		if(getProductLinesID() != null){
-			params.add(getProductLinesID());
 		}
 		params.add(getPriceListVersionID());
 		return params.toArray();
