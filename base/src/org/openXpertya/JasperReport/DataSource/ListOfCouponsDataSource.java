56c43b85b548b1c731f991f7df6bc2a2f6798919
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2017-02-10 22:50:55 +0000

    Merge de Revision 1785

--- base/src/org/openXpertya/JasperReport/DataSource/ListOfCouponsDataSource.java
+++ base/src/org/openXpertya/JasperReport/DataSource/ListOfCouponsDataSource.java
@@ -2,13 +2,15 @@ package org.openXpertya.JasperReport.DataSource;
 
 import java.util.Properties;
 
+import org.openXpertya.model.MBPartner;
+import org.openXpertya.model.MCouponsSettlements;
+import org.openXpertya.model.MCreditCardSettlement;
+import org.openXpertya.model.MEntidadFinanciera;
+import org.openXpertya.model.MEntidadFinancieraPlan;
+import org.openXpertya.model.MPayment;
+import org.openXpertya.model.MReference;
 import org.openXpertya.model.X_AD_Ref_List;
 import org.openXpertya.model.X_AD_Ref_List_Trl;
-import org.openXpertya.model.X_AD_Reference;
-import org.openXpertya.model.X_C_CouponsSettlements;
-import org.openXpertya.model.X_C_CreditCardSettlement;
-import org.openXpertya.model.X_C_Payment;
-import org.openXpertya.model.X_M_EntidadFinanciera;
 import org.openXpertya.util.Env;
 
 /**
@@ -19,7 +21,7 @@ public class ListOfCouponsDataSource extends QueryDataSource {
 	/** Contexto de ejecución. */
 	private Properties ctx;
 
-	private int M_EntidadFinanciera_ID;
+	private int C_BPartner_ID;
 	private String auditState;
 	private String dateFrom;
 	private String dateTo;
@@ -34,35 +36,44 @@ public class ListOfCouponsDataSource extends QueryDataSource {
 		StringBuffer sql = new StringBuffer();
 
 		sql.append("SELECT ");
-		sql.append("	ef.name, ");
-		sql.append("	c.paymentbatch, ");
-		sql.append("	s.settlementno, ");
-		sql.append("	c.couponno, ");
-		sql.append("	c.trxdate, ");
+		sql.append("	bp.name, ");
+		sql.append("	p.couponbatchnumber, ");
+		sql.append("	COALESCE(s.settlementno, '') AS settlementno, ");
+		sql.append("	p.couponnumber AS couponno, ");
+		sql.append("	p.datetrx AS trxdate, ");
 		sql.append("	l.name AS auditstatus, ");
-		sql.append("	c.amount ");
+		sql.append("	p.payamt AS amount ");
 		sql.append("FROM ");
-		sql.append("	" + X_C_CreditCardSettlement.Table_Name + " s ");
-		sql.append("	JOIN " + X_C_CouponsSettlements.Table_Name + " c ON s.c_creditcardsettlement_id = c.c_creditcardsettlement_id ");
-		sql.append("	JOIN " + X_C_Payment.Table_Name + " p ON p.c_payment_id = c.c_payment_id ");
-		sql.append("	JOIN " + X_M_EntidadFinanciera.Table_Name + " ef ON s.m_entidadfinanciera_id = ef.m_entidadfinanciera_id ");
+		sql.append("	" + MPayment.Table_Name + " p ");
+		sql.append("	LEFT JOIN " + MCouponsSettlements.Table_Name + " c ");
+		sql.append("		ON p.c_payment_id = c.c_payment_id ");
+		sql.append("	LEFT JOIN " + MCreditCardSettlement.Table_Name + " s ");
+		sql.append("		ON c.c_creditcardsettlement_id = s.c_creditcardsettlement_id ");
+		sql.append("	LEFT JOIN " + MEntidadFinancieraPlan.Table_Name + " efp ");
+		sql.append("		ON p.m_entidadfinancieraplan_id = efp.m_entidadfinancieraplan_id ");
+		sql.append("	LEFT JOIN " + MEntidadFinanciera.Table_Name + " ef ");
+		sql.append("		ON efp.m_entidadfinanciera_id = ef.m_entidadfinanciera_id ");
+		sql.append("	LEFT JOIN " + MBPartner.Table_Name + " bp ");
+		sql.append("		ON ef.c_bpartner_id = bp.c_bpartner_id ");
 		sql.append("	LEFT JOIN ( ");
 		sql.append("		SELECT ");
 		sql.append("			r.value, ");
 		sql.append("			tr.name ");
 		sql.append("		FROM ");
-		sql.append("			" + X_AD_Reference.Table_Name + " a ");
-		sql.append("			INNER JOIN " + X_AD_Ref_List.Table_Name + " r ON a.ad_reference_id = r.ad_reference_id ");
-		sql.append("			INNER JOIN " + X_AD_Ref_List_Trl.Table_Name + " tr ON tr.ad_ref_list_id = r.ad_ref_list_id ");
+		sql.append("			" + MReference.Table_Name + " a ");
+		sql.append("			INNER JOIN " + X_AD_Ref_List.Table_Name + " r ");
+		sql.append("				ON a.ad_reference_id = r.ad_reference_id ");
+		sql.append("			INNER JOIN " + X_AD_Ref_List_Trl.Table_Name + " tr ");
+		sql.append("				ON tr.ad_ref_list_id = r.ad_ref_list_id ");
 		sql.append("		WHERE ");
 		sql.append("			a.name = 'Audit Status' ");
 		sql.append("			AND tr.ad_language = '" + Env.getAD_Language(ctx) + "' ");
 		sql.append("	) l ON l.value = p.auditstatus ");
 		sql.append("WHERE ");
-		sql.append("	COALESCE(s.settlementno, '') <> '' ");
+		sql.append("	p.TenderType = '" + MPayment.TENDERTYPE_CreditCard + "' ");
 
-		if (M_EntidadFinanciera_ID > 0) {
-			sql.append("	AND s.m_entidadfinanciera_id = " + M_EntidadFinanciera_ID + " ");
+		if (C_BPartner_ID > 0) {
+			sql.append("	AND s.c_bpartner_id = " + C_BPartner_ID + " ");
 		}
 		if (AD_Org_ID > 0) {
 			sql.append("	AND s.ad_org_id = " + AD_Org_ID + " ");
@@ -71,16 +82,16 @@ public class ListOfCouponsDataSource extends QueryDataSource {
 			sql.append("	AND p.auditstatus = '" + auditState + "' ");
 		}
 		if (dateFrom != null && !dateFrom.trim().isEmpty()) {
-			sql.append("	AND s.paymentdate >= '" + dateFrom + "' ");
+			sql.append("	AND p.datetrx >= '" + dateFrom + "' ");
 		}
 		if (dateTo != null && !dateTo.trim().isEmpty()) {
-			sql.append("	AND s.paymentdate <= '" + dateTo + "' ");
+			sql.append("	AND p.datetrx <= '" + dateTo + "' ");
 		}
 
 		sql.append("ORDER BY ");
 		sql.append("	p.auditstatus, ");
 		sql.append("	c.trxdate, ");
-		sql.append("	ef.name, ");
+		sql.append("	bp.name, ");
 		sql.append("	c.paymentbatch, ");
 		sql.append("	s.settlementno, ");
 		sql.append("	c.couponno ");
@@ -100,8 +111,8 @@ public class ListOfCouponsDataSource extends QueryDataSource {
 
 	// SETTERS:
 
-	public void setM_EntidadFinanciera_ID(int m_EntidadFinanciera_ID) {
-		M_EntidadFinanciera_ID = m_EntidadFinanciera_ID;
+	public void setC_BPartner_ID(int C_BPartner_ID) {
+		this.C_BPartner_ID = C_BPartner_ID;
 	}
 
 	public void setDateFrom(String dateFrom) {
