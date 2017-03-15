c8acfb55026557f78921b333301a0645727edd17
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2017-03-15 19:50:34 +0000

    Nueva columna a la view RV_Payment que permite obtener la última asignación relacionada con el payment

--- data/core/upgrade_from_16.04/preinstall_from_16.04.sql
+++ data/core/upgrade_from_16.04/preinstall_from_16.04.sql
@@ -6079,3 +6079,57 @@ where c_doctype_id = (select c_doctype_id from c_doctype where ad_componentobjec
 
 --20170310-1700 Incorporación de flag isprinted a la importación de facturas que permiteindicar facturas ya impresas fiscalmente
 UPDATE ad_system SET dummy = (SELECT addcolumnifnotexists('i_invoice','isprinted','character(1) NOT NULL DEFAULT ''N''::bpchar'));
+
+--20170315-1633 Nueva columna de asignación relacionada al payment
+DROP VIEW rv_payment;
+
+CREATE OR REPLACE VIEW rv_payment AS 
+ SELECT c_payment.c_payment_id, c_payment.ad_client_id, c_payment.ad_org_id, c_payment.isactive, c_payment.created, 
+	c_payment.createdby, c_payment.updated, c_payment.updatedby, c_payment.documentno, c_payment.datetrx, 
+	c_payment.isreceipt, c_payment.c_doctype_id, c_payment.trxtype, c_payment.c_bankaccount_id, 
+	c_payment.c_bpartner_id, c_payment.c_invoice_id, c_payment.c_bp_bankaccount_id, c_payment.c_paymentbatch_id, 
+	c_payment.tendertype, c_payment.creditcardtype, c_payment.creditcardnumber, c_payment.creditcardvv, 
+	c_payment.creditcardexpmm, c_payment.creditcardexpyy, c_payment.micr, c_payment.routingno, c_payment.accountno, 
+	c_payment.checkno, c_payment.a_name, c_payment.a_street, c_payment.a_city, c_payment.a_state, c_payment.a_zip, 
+	c_payment.a_ident_dl, c_payment.a_ident_ssn, c_payment.a_email, c_payment.voiceauthcode, c_payment.orig_trxid, 
+	c_payment.ponum, c_payment.c_currency_id, c_payment.c_conversiontype_id, 
+        CASE c_payment.isreceipt
+            WHEN 'Y'::bpchar THEN c_payment.payamt
+            ELSE c_payment.payamt * (- 1::numeric)
+        END AS payamt, 
+        CASE c_payment.isreceipt
+            WHEN 'Y'::bpchar THEN c_payment.discountamt
+            ELSE c_payment.discountamt * (- 1::numeric)
+        END AS discountamt, 
+        CASE c_payment.isreceipt
+            WHEN 'Y'::bpchar THEN c_payment.writeoffamt
+            ELSE c_payment.writeoffamt * (- 1::numeric)
+        END AS writeoffamt, 
+        CASE c_payment.isreceipt
+            WHEN 'Y'::bpchar THEN c_payment.taxamt
+            ELSE c_payment.taxamt * (- 1::numeric)
+        END AS taxamt, 
+        CASE c_payment.isreceipt
+            WHEN 'Y'::bpchar THEN c_payment.overunderamt
+            ELSE c_payment.overunderamt * (- 1::numeric)
+        END AS overunderamt, 
+        CASE c_payment.isreceipt
+            WHEN 'Y'::bpchar THEN 1.0
+            ELSE (-1.0)
+        END AS multiplierap, 
+        paymentallocated(c_payment.c_payment_id::numeric, c_payment.c_currency_id::numeric) AS allocatedamt, 
+        paymentavailable(c_payment.c_payment_id::numeric) AS availableamt, 
+        c_payment.isoverunderpayment, c_payment.isapproved, c_payment.r_pnref, c_payment.r_result, c_payment.r_respmsg, 
+        c_payment.r_authcode, c_payment.r_avsaddr, c_payment.r_avszip, c_payment.r_info, c_payment.processing, 
+        c_payment.oprocessing, c_payment.docstatus, c_payment.docaction, c_payment.isprepayment, c_payment.c_charge_id, 
+        c_payment.isreconciled, c_payment.isallocated, c_payment.isonline, c_payment.processed, c_payment.posted, 
+        c_payment.dateacct,
+        (select ah.c_allocationhdr_id 
+	from c_allocationhdr ah 
+	inner join c_allocationline al on al.c_allocationhdr_id = ah.c_allocationhdr_id
+	where ah.docstatus in ('CO','CL') and al.c_payment_id = c_payment.c_payment_id
+	order by ah.updated desc
+	limit 1) as c_allocationhdr_id
+   FROM c_payment;
+ALTER TABLE rv_payment
+  OWNER TO libertya;
