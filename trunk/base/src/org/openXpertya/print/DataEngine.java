e640645603e5c506af496522f53fd89f5690eed4
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2017-04-10 12:48:58 +0000

    Fix a consulta para devolver las columnas de las vistas relacionadas con las columnas del formato de impresión

--- trunk/base/src/org/openXpertya/print/DataEngine.java
+++ trunk/base/src/org/openXpertya/print/DataEngine.java
@@ -246,8 +246,12 @@ public class DataEngine {
                      + "pfi.IsMinCalc,pfi.IsMaxCalc, "                                                                                                                                                                                                                                                                                                                    // 18..19
                      + "pfi.isRunningTotal,pfi.RunningTotalLines, "                                                                                                                                                                                                                                                                                                       // 20..21
                      + "pfi.IsVarianceCalc, pfi.IsDeviationCalc "                                                                                                                                                                                                                                                                                                         // 22..23
-                     + "FROM AD_PrintFormat pf" + " INNER JOIN AD_PrintFormatItem pfi ON (pf.AD_PrintFormat_ID=pfi.AD_PrintFormat_ID)" + " INNER JOIN AD_Column c ON (pfi.AD_Column_ID=c.AD_Column_ID)" + " LEFT OUTER JOIN AD_ReportView_Col rvc ON (pf.AD_ReportView_ID=rvc.AD_ReportView_ID AND c.AD_Column_ID=rvc.AD_Column_ID) " + "WHERE pf.AD_PrintFormat_ID=?"    // #1
-                     + " AND pfi.IsActive='Y' AND (pfi.IsPrinted='Y' OR c.IsKey='Y' OR pfi.SortNo > 0) " + "ORDER BY pfi.IsPrinted DESC, pfi.SeqNo";    // Functions are put in first column
+                     + "FROM AD_PrintFormat pf" 
+                     + " INNER JOIN AD_PrintFormatItem pfi ON (pf.AD_PrintFormat_ID=pfi.AD_PrintFormat_ID)" 
+                     + " INNER JOIN AD_Column c ON (pfi.AD_Column_ID=c.AD_Column_ID)" 
+                     + " LEFT JOIN AD_ReportView_Col rvc ON (pf.AD_ReportView_ID=rvc.AD_ReportView_ID AND c.AD_Column_ID=rvc.AD_Column_ID) " + "WHERE pf.AD_PrintFormat_ID=?"    // #1
+                     + " AND pfi.IsActive='Y' AND (pfi.IsPrinted='Y' OR c.IsKey='Y' OR pfi.SortNo > 0) " 
+                     + "ORDER BY pfi.IsPrinted DESC, pfi.SeqNo";    // Functions are put in first column
 
         try {
             PreparedStatement pstmt = DB.prepareStatement( sql );
