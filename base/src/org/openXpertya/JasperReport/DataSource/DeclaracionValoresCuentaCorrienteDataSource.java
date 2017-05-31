b3c5c17b880b66f758d120f974bc594ac1cd2c2d
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2017-05-31 23:37:09 +0000

    Se omiten las asignaciones anuladas para la sección de cuentas corrientes

--- base/src/org/openXpertya/JasperReport/DataSource/DeclaracionValoresCuentaCorrienteDataSource.java
+++ base/src/org/openXpertya/JasperReport/DataSource/DeclaracionValoresCuentaCorrienteDataSource.java
@@ -30,7 +30,7 @@ public class DeclaracionValoresCuentaCorrienteDataSource extends
 																								"inner join c_allocationline as al on al.c_allocationhdr_id = ah.c_allocationhdr_id " +
 																								"where ah.isactive = 'Y' " +
 																								"group by c_invoice_id, c_posjournal_id) as ds on ds.alloc_invoice_id = dv.c_invoice_id and dv.c_posjournal_id = ds.alloc_journal_id WHERE ");
-		sql.append(getStdWhereClause(true,"dv",true,false));
+		sql.append(getStdWhereClause(true,"dv",true,true));
 		superSql.append(sql);
 		superSql.append(") as inv ");
 		superSql.append(" WHERE (open > 0) ");
