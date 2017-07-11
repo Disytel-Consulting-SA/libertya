3db2e8e899357161eabfe7ffe8fbb6335d691e5d
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2017-07-11 12:48:44 +0000

    Bug fix: Omisión de signo de negación en la condición al completar retenciones

--- base/src/org/openXpertya/process/RetencionFacturasMIva.java
+++ base/src/org/openXpertya/process/RetencionFacturasMIva.java
@@ -262,7 +262,7 @@ public class RetencionFacturasMIva extends AbstractRetencionProcessor {
 			 throw new Exception( "@CollectorInvoiceLineSaveError@: "+CLogger.retrieveErrorAsString());
 		
 		/*Completo la factura*/
-		if(DocumentEngine.processAndSave(recaudador_fac, MInvoice.DOCACTION_Complete, false)){
+		if(!DocumentEngine.processAndSave(recaudador_fac, MInvoice.DOCACTION_Complete, false)){
 			throw new Exception(recaudador_fac.getProcessMsg());
 		}
 		
@@ -338,7 +338,7 @@ public class RetencionFacturasMIva extends AbstractRetencionProcessor {
 			 throw new Exception( "@VendorRetencionDocLineSaveError@: "+CLogger.retrieveErrorAsString());
 		
 		/*Completo la factura*/
-		if(DocumentEngine.processAndSave(credito_prov, MInvoice.DOCACTION_Complete, false)){
+		if(!DocumentEngine.processAndSave(credito_prov, MInvoice.DOCACTION_Complete, false)){
 			throw new Exception(credito_prov.getProcessMsg());
 		}
 		retencion.setC_InvoiceLine_ID(cred_linea.getC_InvoiceLine_ID());
