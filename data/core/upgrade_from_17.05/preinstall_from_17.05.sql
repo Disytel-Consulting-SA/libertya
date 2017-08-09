affdc3487b93ec44d3ac0e433896e9a5796b681b
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2017-08-09 14:59:23 +0000

    Fix a función paymentavailable ya que se encontraron casos donde existen allocations negativos

--- data/core/upgrade_from_17.05/preinstall_from_17.05.sql
+++ data/core/upgrade_from_17.05/preinstall_from_17.05.sql
@@ -213,3 +213,71 @@ update ad_system set dummy = (SELECT addcolumnifnotexists('i_invoice','datecai',
 --20170729-1225 Por defecto se setean a N las columnas Cliente y Cliente Potencial
 ALTER TABLE C_BPARTNER ALTER COLUMN iscustomer SET DEFAULT 'N'::bpchar;
 ALTER TABLE C_BPARTNER ALTER COLUMN isprospect SET DEFAULT 'N'::bpchar;
+
+--20170809-1158 Se encontraron casos de allocations en negativo
+CREATE OR REPLACE FUNCTION paymentavailable(
+    p_c_payment_id integer,
+    dateto timestamp without time zone)
+  RETURNS numeric AS
+$BODY$
+DECLARE
+v_Currency_ID INTEGER;
+v_AvailableAmt NUMERIC := 0;
+  v_IsReceipt CHARACTER(1);
+  v_Amt NUMERIC := 0;
+  r RECORD;
+v_Charge_ID INTEGER;
+v_ConversionType_ID INTEGER;
+v_allocatedAmt NUMERIC; -- candida alocada total convertida a la moneda de la linea
+v_DateAcct timestamp without time zone;
+BEGIN
+BEGIN
+
+SELECT C_Currency_ID, PayAmt, IsReceipt,
+C_Charge_ID,C_ConversionType_ID, DateAcct
+INTO STRICT
+v_Currency_ID, v_AvailableAmt, v_IsReceipt,
+v_Charge_ID,v_ConversionType_ID, v_DateAcct
+FROM C_Payment
+WHERE C_Payment_ID = p_C_Payment_ID;
+EXCEPTION
+WHEN OTHERS THEN
+  RAISE NOTICE 'PaymentAvailable - %', SQLERRM;
+RETURN NULL;
+END;
+
+IF (v_Charge_ID > 0 ) THEN
+RETURN 0;
+END IF;
+
+v_allocatedAmt := 0;
+FOR r IN
+SELECT a.AD_Client_ID, a.AD_Org_ID, al.Amount, a.C_Currency_ID, a.DateTrx
+FROM C_AllocationLine al
+INNER JOIN C_AllocationHdr a ON (al.C_AllocationHdr_ID=a.C_AllocationHdr_ID)
+WHERE al.C_Payment_ID = p_C_Payment_ID
+  AND a.IsActive='Y'
+  AND (dateTo IS NULL OR a.dateacct::date <= dateTo::date)
+LOOP
+v_Amt := currencyConvert(r.Amount, r.C_Currency_ID, v_Currency_ID,
+v_DateAcct, v_ConversionType_ID, r.AD_Client_ID, r.AD_Org_ID);
+v_allocatedAmt := v_allocatedAmt + v_Amt;
+END LOOP;
+
+-- esto supone que las alocaciones son siempre no negativas; si esto no pasa, se van a retornar valores que no van a tener sentido
+v_AvailableAmt := ABS(v_AvailableAmt) - abs(v_allocatedAmt);
+-- v_AvailableAmt aca DEBE ser NO Negativo si admeas, las suma de las alocaciones nunca superan el monto del pago
+-- de cualquiera manera, por "seguridad", si el valor es negativo, se corrige a cero
+IF (v_AvailableAmt < 0) THEN
+RAISE NOTICE 'Payment Available negative, correcting to zero - %',v_AvailableAmt ;
+v_AvailableAmt := 0;
+END IF;
+
+v_AvailableAmt := currencyRound(v_AvailableAmt,v_Currency_ID,NULL);
+RETURN v_AvailableAmt;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION paymentavailable(integer, timestamp without time zone)
+  OWNER TO libertya;
