c7d5ae071827bce3bb69f1ea53354d1fe407224b
fcristina <fcristina@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2018-08-21 12:16:51 +0000

    Modificaciones en query para mejora de performance en los tiempos de respuesta.  Cruza con allocations recien luego de obtener c_pos_declaracionvalores.

--- trunk/base/src/org/openXpertya/JasperReport/DataSource/DeclaracionValoresCuentaCorrienteDataSource.java
+++ trunk/base/src/org/openXpertya/JasperReport/DataSource/DeclaracionValoresCuentaCorrienteDataSource.java
@@ -23,13 +23,9 @@ public class DeclaracionValoresCuentaCorrienteDataSource extends
 	protected String getQuery() {
 		// Monto pendiente de la facturas
 		StringBuffer superSql = new StringBuffer("SELECT invoice_documentno, invoice_grandtotal, open as ingreso, egreso, open as total   FROM (");
-		StringBuffer sql = new StringBuffer("SELECT dv.*,invoice_grandtotal-coalesce(ds.amount,0) as open  " +
+		StringBuffer sql = new StringBuffer("SELECT dv.*,invoice_grandtotal-coalesce((select sum(amount+writeoffamt) as amount from c_allocationhdr as ah inner join c_allocationline as al on al.c_allocationhdr_id = ah.c_allocationhdr_id where ah.isactive = 'Y' and c_invoice_id = dv.c_invoice_id and dv.c_posjournal_id = c_posjournal_id group by c_invoice_id, c_posjournal_id),0) as open  " +
 											"FROM "+getDSFunView("c_pos_declaracionvalores_ventas_filtered")+" as dv " +
-											"LEFT JOIN (select c_invoice_id as alloc_invoice_id, c_posjournal_id as alloc_journal_id, sum(amount+writeoffamt) as amount " +
-																								"from c_allocationhdr as ah " +
-																								"inner join c_allocationline as al on al.c_allocationhdr_id = ah.c_allocationhdr_id " +
-																								"where ah.isactive = 'Y' " +
-																								"group by c_invoice_id, c_posjournal_id) as ds on ds.alloc_invoice_id = dv.c_invoice_id and dv.c_posjournal_id = ds.alloc_journal_id WHERE ");
+											"WHERE ");
 		sql.append(getStdWhereClause(true,"dv",true,false));
 		sql.append(" AND (allocation_active is null OR allocation_active = 'Y') ");
 		superSql.append(sql);
