45f8d9be2c776648023bee2c9266d28ee590fc52
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2018-09-06 17:17:12 +0000

    Habilitación de EC para transacciones se valida también al guardar transacciones

--- base/src/org/openXpertya/model/BusinessPartnerValidator.java
+++ base/src/org/openXpertya/model/BusinessPartnerValidator.java
@@ -13,6 +13,21 @@ public class BusinessPartnerValidator implements ModelValidator {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Validación de habilitación de la entidad comercial para transacciones
+	 * 
+	 * @param bp entidad comercial
+	 * @return resultado de la llamada
+	 */
+	protected CallResult validateTrxEnabled(MBPartner bp) {
+		CallResult cr = new CallResult();
+		if(!bp.isTrxEnabled()){
+			cr.setMsg(Msg.getMsg(bp.getCtx(), "BPartnerTrxDisabled",
+					new Object[] { bp.getValue() + " - " + bp.getName() }), true);
+		}
+		return cr;
+	}
+	
 	@Override
 	public String docValidate(PO po, int timing) {
 		// Para este validator, la EC es obligatoria
@@ -25,9 +40,9 @@ public class BusinessPartnerValidator implements ModelValidator {
 			MBPartner bp = new MBPartner(po.getCtx(), bPartnerID, po.get_TrxName());
 			// Si la EC relacionada al documento no esta habilitada para transacciones, 
 			// no se puede trabajar sobre ningun comprobante
-			if(!bp.isTrxEnabled()){
-				return Msg.getMsg(bp.getCtx(), "BPartnerTrxDisabled",
-						new Object[] { bp.getValue() + " - " + bp.getName() });
+			CallResult cr = validateTrxEnabled(bp);
+			if(cr.isError()) {
+				return cr.getMsg();
 			}
 		}
 		return null;
@@ -36,6 +51,7 @@ public class BusinessPartnerValidator implements ModelValidator {
 	@Override
 	public void initialize(ModelValidationEngine engine, MClient client) {
 		setAD_Client_ID(client.getID());
+		// Documentos
 		engine.addDocValidate(X_C_Order.Table_Name, this);
 		engine.addDocValidate(X_C_Invoice.Table_Name, this);
 		engine.addDocValidate(X_C_Payment.Table_Name, this);
@@ -46,6 +62,17 @@ public class BusinessPartnerValidator implements ModelValidator {
 		engine.addDocValidate(X_M_Inventory.Table_Name, this);
 		engine.addDocValidate(X_M_Movement.Table_Name, this);
 		engine.addDocValidate(X_M_Transfer.Table_Name, this);
+		// Modelo
+		engine.addModelChange(X_C_Order.Table_Name, this);
+		engine.addModelChange(X_C_Invoice.Table_Name, this);
+		engine.addModelChange(X_C_Payment.Table_Name, this);
+		engine.addModelChange(X_C_CashLine.Table_Name, this);
+		engine.addModelChange(X_C_AllocationHdr.Table_Name, this);
+		engine.addModelChange(X_C_BankStatement.Table_Name, this);
+		engine.addModelChange(X_M_InOut.Table_Name, this);
+		engine.addModelChange(X_M_Inventory.Table_Name, this);
+		engine.addModelChange(X_M_Movement.Table_Name, this);
+		engine.addModelChange(X_M_Transfer.Table_Name, this);
 	}
 
 	@Override
@@ -62,7 +89,21 @@ public class BusinessPartnerValidator implements ModelValidator {
 
 	@Override
 	public String modelChange(PO po, int type) throws Exception {
-		// TODO Auto-generated method stub
+		// Para este validator, la EC es obligatoria
+		Integer bPartnerID = (Integer)po.get_Value("C_BPartner_ID");
+		if(Util.isEmpty(bPartnerID, true)){
+			return null;
+		}
+		// En prepareIt
+		if(type == TYPE_NEW || type == TYPE_CHANGE){
+			MBPartner bp = new MBPartner(po.getCtx(), bPartnerID, po.get_TrxName());
+			// Si la EC relacionada al documento no esta habilitada para transacciones, 
+			// no se puede trabajar sobre ningun comprobante
+			CallResult cr = validateTrxEnabled(bp);
+			if(cr.isError()) {
+				return cr.getMsg();
+			}
+		}
 		return null;
 	}
 
