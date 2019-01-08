2a9e5dd09679b72a7f2929b70ba4c35cc56b657c
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2019-01-08 18:49:02 +0000

    Columna nro_liquidacion no existe

--- base/src/org/openXpertya/process/customImport/centralPos/mapping/extras/NaranjaInvoicedConcepts.java
+++ base/src/org/openXpertya/process/customImport/centralPos/mapping/extras/NaranjaInvoicedConcepts.java
@@ -14,7 +14,6 @@ public class NaranjaInvoicedConcepts extends GenericMap {
 	public static String[] filteredFields = {
 			// Asociación con Header
 			"comercio", // Comercio.
-			"nro_liquidacion", 
 			"fecha_pago", // Fecha de pago.
 			// Comisiones
 			"importe_ara_vto", // Importe descontado de arancel facturado en vto.
@@ -63,7 +62,7 @@ public class NaranjaInvoicedConcepts extends GenericMap {
 
 	public NaranjaInvoicedConcepts(Datum values) {
 		super(filteredFields, values, null);
-		matchingFields = new String[] { "comercio", "nro_liquidacion", "fecha_pago" };
+		matchingFields = new String[] { "comercio", "fecha_pago" };
 	}
 
 }
