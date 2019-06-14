76a058f5c2c3ebd5e3f2bc239070ed00bc7f6a25
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2019-06-14 18:01:07 +0000

    Eliminación de filtro por condición Cuenta Corriente para facturas ya que trae problemas en la visualización de los comprobantes manuales

--- trunk/base/src/org/openXpertya/JasperReport/DataSource/DeclaracionValoresCuentaCorrienteDataSource.java
+++ trunk/base/src/org/openXpertya/JasperReport/DataSource/DeclaracionValoresCuentaCorrienteDataSource.java
@@ -24,12 +24,10 @@ public class DeclaracionValoresCuentaCorrienteDataSource extends
 		// Monto pendiente de la facturas
 		StringBuffer superSql = new StringBuffer("SELECT invoice_documentno, invoice_grandtotal, open as ingreso, egreso, open as total   FROM (");
 		StringBuffer sql = new StringBuffer("SELECT dv.*,invoice_grandtotal-coalesce((select sum(amount+writeoffamt) as amount from c_allocationhdr as ah inner join c_allocationline as al on al.c_allocationhdr_id = ah.c_allocationhdr_id where ah.isactive = 'Y' and c_invoice_id = dv.c_invoice_id and dv.c_posjournal_id = c_posjournal_id group by c_invoice_id, c_posjournal_id),0) as open  " +
-											"FROM "+super.getDSDataTable()+" as dv " +
-											"JOIN c_invoice i on i.c_invoice_id = dv.c_invoice_id " + 
+											"FROM "+super.getDSDataTable()+" as dv " + 
 											"WHERE ");
 		sql.append(getStdWhereClause(true,"dv",true,false));
 		sql.append(" AND (allocation_active is null OR allocation_active = 'Y') ");
-		sql.append(" AND (i.paymentrule = 'P') ");
 		superSql.append(sql);
 		superSql.append(") as inv ");
 		superSql.append(" WHERE (open > 0) ");
