aef0ec5a57010db1d87df30cdb2679db7add7643
fcristina <fcristina@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2019-07-26 13:13:27 +0000

    Estructura de desarrollo para proximo release

---
+++ trunk/client/Src/org/openXpertya/process/release/PostInstallUpgradeFrom1907.java
@@ -0,0 +1,25 @@
+package org.openXpertya.process.release;
+
+import org.openXpertya.process.PluginPostInstallProcess;
+
+public class PostInstallUpgradeFrom1907 extends PluginPostInstallProcess {
+
+	
+	@Override
+	protected String doIt() throws Exception {
+		super.doIt();
+		
+		/*
+		 * Actualizacion de binarios
+		 * """"""""""""""""""""""""" 
+		 * Utilizar SIEMPRE los métodos MJasperReport.updateBinaryData() y MProcess.addAttachment() 
+		 * para la carga de informes tipo Jasper, el primero para la carga en AD_JasperReport y el 
+		 * segundo en reportes dinámicos, los cuales van adjuntos en el informe/proceso correspondiente.
+		 */
+		
+		
+		return " ";
+	}
+	
+
+}
