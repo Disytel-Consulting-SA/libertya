b142c180f38e7dc91a4d144968f5a543d9a0a657
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2019-08-08 15:19:22 +0000

    Incorporación de Precio Histórico al informe Detalle de movimientos vaorizados

--- base/src/org/openXpertya/JasperReport/DataSource/ValuedMovementsDetailDataSource.java
+++ base/src/org/openXpertya/JasperReport/DataSource/ValuedMovementsDetailDataSource.java
@@ -6,11 +6,15 @@ import java.util.List;
 import java.util.Properties;
 
 import org.openXpertya.util.Env;
+import org.openXpertya.util.Util;
 
 public class ValuedMovementsDetailDataSource extends ValuedMovementsDataSource {
 
 	/** Línea de Artículo */
 	private Integer productLinesID;
+	
+	/** Tipo de precio */
+	private String priceType;
 	
 	public ValuedMovementsDetailDataSource(String trxName) {
 		super(trxName);
@@ -26,17 +30,18 @@ public class ValuedMovementsDetailDataSource extends ValuedMovementsDataSource {
 	public ValuedMovementsDetailDataSource(Properties ctx, Integer orgID,
 			Timestamp dateFrom, Timestamp dateTo, Integer warehouseID,
 			Integer priceListVersionID, Integer chargeID, Integer productLinesID, 
-			String trxName) {
+			String priceType, String trxName) {
 		super(ctx, orgID, dateFrom, dateTo, warehouseID, priceListVersionID,
 				chargeID, trxName);
 		setProductLinesID(productLinesID);
+		setPriceType(priceType);
 	}
 
 	@Override
 	protected String getQuery() {
 		StringBuffer sql = new StringBuffer("SELECT tablename, ad_org_id, orgvalue, orgname, doc_id, documentno, datetrx, description, c_charge_id, chargename, m_warehouse_id, warehousevalue, warehousename, m_warehouseto_id, warehousetovalue, warehousetoname, m_product_id, productvalue, productname, productlinesvalue, productlinesname, pricestd, sum(qty) as qty, sum(amt) as amt " +
-											" FROM (SELECT tablename, m.ad_org_id, orgvalue, orgname, doc_id, documentno, datetrx, description, c_charge_id, chargename, m_warehouse_id, warehousevalue, warehousename, m_warehouseto_id, warehousetovalue, warehousetoname, m.m_product_id, productvalue, productname, productlinesvalue, productlinesname, qty, coalesce(pp.pricestd,0) as pricestd, coalesce(pp.pricestd,0) * m.qty as amt " +
-											" FROM (SELECT tablename, ad_org_id, orgvalue, orgname, doc_id, documentno, datetrx, description, c_charge_id, chargename, m_warehouse_id, warehousevalue, warehousename, m_warehouseto_id, warehousetovalue, warehousetoname, m_product_id, productvalue, productname, productlinesvalue, productlinesname, qty " +
+											" FROM (SELECT tablename, m.ad_org_id, orgvalue, orgname, doc_id, documentno, datetrx, description, c_charge_id, chargename, m_warehouse_id, warehousevalue, warehousename, m_warehouseto_id, warehousetovalue, warehousetoname, m.m_product_id, productvalue, productname, productlinesvalue, productlinesname, qty, coalesce("+getPriceColumnName()+",0) as pricestd, coalesce("+getPriceColumnName()+",0) * m.qty as amt " +
+											" FROM (SELECT tablename, ad_org_id, orgvalue, orgname, doc_id, documentno, datetrx, description, c_charge_id, chargename, m_warehouse_id, warehousevalue, warehousename, m_warehouseto_id, warehousetovalue, warehousetoname, m_product_id, productvalue, productname, productlinesvalue, productlinesname, qty, coalesce(costprice,0) as costprice " +
 											"		FROM v_product_movements_filtered( " + (getOrgID() != null ? getOrgID() : "-1") + ", "
 																						 + (getDateFrom() != null ? "'" + getDateFrom() + "'" : "null") + "::timestamp, "
 																						 + (getDateTo() != null ? "'" + getDateTo() + "'" : "null") + "::timestamp, "
@@ -50,9 +55,12 @@ public class ValuedMovementsDetailDataSource extends ValuedMovementsDataSource {
 			sql.append(" AND ((tablename = 'M_Transfer' AND type = 'T' AND aditionaltype = 'I' AND m_warehouseto_id = ?) OR m_warehouse_id = ?) ");
 		}
 		sql.append(") as m ");
-		sql.append("LEFT JOIN m_productprice as pp on pp.m_product_id = m.m_product_id " +
-				   "WHERE pp.m_pricelist_version_id = ? ) as a " +
-				   "GROUP BY tablename, ad_org_id, orgvalue, orgname, doc_id, documentno, datetrx, description, c_charge_id, chargename, m_warehouse_id, warehousevalue, warehousename, m_warehouseto_id, warehousetovalue, warehousetoname, m_product_id, productvalue, productname, productlinesvalue, productlinesname, pricestd ");
+		if(!Util.isEmpty(getPriceListVersionID())){
+			sql.append(" LEFT JOIN m_productprice as pp on pp.m_product_id = m.m_product_id " +
+						" WHERE pp.m_pricelist_version_id = ? ");
+		}
+		sql.append(" ) as a ");
+		sql.append(" GROUP BY tablename, ad_org_id, orgvalue, orgname, doc_id, documentno, datetrx, description, c_charge_id, chargename, m_warehouse_id, warehousevalue, warehousename, m_warehouseto_id, warehousetovalue, warehousetoname, m_product_id, productvalue, productname, productlinesvalue, productlinesname, pricestd ");
 		sql.append(" ORDER BY orgvalue, datetrx, tablename, documentno ");
 		return sql.toString();
 	}
@@ -65,7 +73,9 @@ public class ValuedMovementsDetailDataSource extends ValuedMovementsDataSource {
 			params.add(getWarehouseID());
 			params.add(getWarehouseID());
 		}
-		params.add(getPriceListVersionID());
+		if(!Util.isEmpty(getPriceListVersionID())){
+			params.add(getPriceListVersionID());
+		}
 		return params.toArray();
 	}
 
@@ -80,5 +90,20 @@ public class ValuedMovementsDetailDataSource extends ValuedMovementsDataSource {
 	@Override
 	protected String getDescriptionByTable(){
 		return (String)getCurrentRecord().get("DESCRIPTION");
+	}
+
+	public String getPriceType() {
+		return priceType;
+	}
+
+	public void setPriceType(String priceType) {
+		this.priceType = priceType;
+	}
+	
+	/**
+	 * @return columna de precio a utilizar, precio de tarifa o histórico
+	 */
+	public String getPriceColumnName(){
+		return getPriceType().equals("P")?"pricestd":"costprice";
 	}
 }
