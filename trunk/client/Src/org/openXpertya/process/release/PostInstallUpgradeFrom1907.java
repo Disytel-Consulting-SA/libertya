aaffd09d45032f47aa07cc9d1eb3bf9cffe58ecf
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2019-08-08 16:46:54 +0000

    Incorporación del informe Detalle de Movimientos Valorizados en el postinstall

--- trunk/client/Src/org/openXpertya/process/release/PostInstallUpgradeFrom1907.java
+++ trunk/client/Src/org/openXpertya/process/release/PostInstallUpgradeFrom1907.java
@@ -1,9 +1,14 @@
 package org.openXpertya.process.release;
 
+import org.openXpertya.JasperReport.MJasperReport;
 import org.openXpertya.process.PluginPostInstallProcess;
+import org.openXpertya.utils.JarHelper;
 
 public class PostInstallUpgradeFrom1907 extends PluginPostInstallProcess {
 
+	/** UID del informe de Movimientos Valorizados Detallado */
+	protected final static String VALUED_MOVEMENTS_DETAIL_JASPER_REPORT_UID = "CORE-AD_JasperReport-1010117";
+	protected final static String VALUED_MOVEMENTS_DETAIL_JASPER_REPORT_FILENAME = "ValuedMovementsDetail.jasper";
 	
 	@Override
 	protected String doIt() throws Exception {
@@ -16,6 +21,17 @@ public class PostInstallUpgradeFrom1907 extends PluginPostInstallProcess {
 		 * para la carga de informes tipo Jasper, el primero para la carga en AD_JasperReport y el 
 		 * segundo en reportes dinámicos, los cuales van adjuntos en el informe/proceso correspondiente.
 		 */
+		
+		// Reporte de Movimientos Valorizados Detallado
+		MJasperReport
+			.updateBinaryData(
+					get_TrxName(),
+					getCtx(),
+					VALUED_MOVEMENTS_DETAIL_JASPER_REPORT_UID,
+					JarHelper
+							.readBinaryFromJar(
+									jarFileURL,
+									getBinaryFileURL(VALUED_MOVEMENTS_DETAIL_JASPER_REPORT_FILENAME)));
 		
 		
 		return " ";
