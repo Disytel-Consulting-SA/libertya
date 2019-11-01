3cb43252f555e2fd5a0661bb5a256564af487e73
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2019-11-01 14:59:33 +0000

    Mejoras y correcciones al informe de balance contable aplicando índices por inflación

--- base/src/org/openXpertya/process/AccountsGeneralBalance.java
+++ base/src/org/openXpertya/process/AccountsGeneralBalance.java
@@ -53,51 +53,25 @@ public class AccountsGeneralBalance extends AccountsHierarchicalReport {
 		super.doIt();
 		
 		// Obtener el índice de inflación entre la fecha desde y hasta
-		BigDecimal inflationIndex = getInflationIndex();
-		
-		StringBuffer sqlView = new StringBuffer();
-		sqlView.append(" SELECT ev.C_ElementValue_ID, tb.C_ElementValue_To_ID, tb.HierarchicalCode, COALESCE(SUM(fa.AmtAcctDr),0) as AmtAcctDr, COALESCE(SUM(fa.AmtAcctCr),0) as AmtAcctCr "); 
-		
-		sqlView.append(" FROM C_ElementValue ev ");
-		sqlView.append(" LEFT JOIN "+p_factAcctTable+" fa ON (fa.Account_ID = ev.C_ElementValue_ID) ");
-		sqlView.append(" INNER JOIN " + getReportTableName() + " tb ON (tb.C_ElementValue_ID = ev.C_ElementValue_ID AND tb.AD_PInstance_ID = ?) ");
-		sqlView.append(" WHERE ev.IsActive = 'Y' ");
-		sqlView.append("   AND fa.AD_Client_ID = ").append(getAD_Client_ID());
-		sqlAppend     ("   AND fa.AD_Org_ID = ? ", p_AD_Org_ID > 0, sqlView);
-		sqlAppend     ("   AND ?::date <= fa.DateAcct::date ", p_DateAcct_From, sqlView);
-		sqlAppend     ("   AND fa.DateAcct::date <= ?::date ", p_DateAcct_To, sqlView);
-		sqlView.append(" GROUP BY ev.C_ElementValue_ID, tb.C_ElementValue_To_ID, tb.HierarchicalCode ");
+		MInflationIndex inflationIndex = getInflationIndex();
 		
 		StringBuffer sql = new StringBuffer();
 		
 		sql.append(" UPDATE " + getReportTableName() + " t ");
 		sql.append(" SET Debit = ( ");
-		sql.append(" 	    SELECT COALESCE(SUM(AmtAcctDr),0.0) AS Debit ");
+		sql.append(" 	    SELECT COALESCE(SUM(thesum),0.0) AS Debit ");
 		
-		sql.append(" 	    FROM ( ").append(sqlView).append(" ) v ");
+		sql.append(" 	    FROM ( ").append(getSQLView("AmtAcctDr", false)).append(" ) v ");
 		sql.append(" 	    WHERE  v.HierarchicalCode LIKE t.HierarchicalCode || '%' ");
 		sql.append("     ), ");
 		sql.append("     Credit = ( ");
-		sql.append(" 	    SELECT COALESCE(SUM(AmtAcctCr),0.0) AS Credit ");
+		sql.append(" 	    SELECT COALESCE(SUM(thesum),0.0) AS Credit ");
 		
-		sql.append(" 	    FROM ( ").append(sqlView).append(" ) v ");
+		sql.append(" 	    FROM ( ").append(getSQLView("AmtAcctCr", false)).append(" ) v ");
 		sql.append(" 	    WHERE  v.HierarchicalCode LIKE t.HierarchicalCode || '%' ");
 		sql.append("     ) ");
+		
 		sql.append(" WHERE t.AD_PInstance_ID = ? ");
-		
-		
-		StringBuffer sqlUpdateBalance = new StringBuffer();
-		sqlUpdateBalance.append(" UPDATE ").append(getReportTableName());
-		sqlUpdateBalance.append(" SET Balance = Debit - Credit ");
-		if (p_C_ElementValue_To_ID != null) {
-			sqlUpdateBalance.append(", ");
-			sqlUpdateBalance.append(" c_elementvalue_to_id = " + p_C_ElementValue_To_ID );
-		}
-		// Saldo ajustado
-		sqlUpdateBalance.append(", ");
-		sqlUpdateBalance.append(" BalanceAdjusted = (CASE WHEN isadjustable = 'N' THEN (Debit - Credit) ELSE ((Debit - Credit) + ((Debit - Credit) * ")
-				.append(applyInflationIndexes ? inflationIndex : "0").append(")) END) ");
-		sqlUpdateBalance.append(" WHERE AD_PInstance_ID = ? ");
 		
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -126,12 +100,59 @@ public class AccountsGeneralBalance extends AccountsHierarchicalReport {
 			
 			// Actualización de saldo
 			if(updateBalance){
+				// Actualizar el saldo ajustado
+				StringBuffer sqlUpdateBalance = new StringBuffer();
+				if(applyInflationIndexes) {
+					sqlUpdateBalance.append(" UPDATE ").append(getReportTableName()).append(" t ");
+					sqlUpdateBalance.append(" SET BalanceAdjusted =  ");
+					sqlUpdateBalance.append(" 	    (SELECT COALESCE(SUM(thesum),0.0) AS thesum ");
+					
+					sqlUpdateBalance.append(" 	    FROM ( ")
+							.append(getSQLView("(AmtAcctDr-AmtAcctCr)+((AmtAcctDr-AmtAcctCr) * (CASE WHEN "
+									+ inflationIndex.getInflationIndex() + " = 0 THEN 0 ELSE ((inflationindex - "
+									+ inflationIndex.getInflationIndex() + ") / " + inflationIndex.getInflationIndex()
+									+ ") END))", true))
+							.append(" ) v ");
+					sqlUpdateBalance.append(" 	    WHERE  v.HierarchicalCode LIKE t.HierarchicalCode || '%' ");
+					sqlUpdateBalance.append("     ) ");
+					sqlUpdateBalance.append(" WHERE AD_PInstance_ID = ? ");
+					pstmt = DB.prepareStatement(sqlUpdateBalance.toString(), get_TrxName(), true);
+					i = 1;
+					pstmt.setInt     (i++, getAD_PInstance_ID());
+					i = pstmtSetParam(i, auxAD_Org_ID, pstmt);
+					i = pstmtSetParam(i, p_DateAcct_From, pstmt);
+					i = pstmtSetParam(i, p_DateAcct_To, pstmt);
+					pstmt.setInt(i++, getAD_PInstance_ID());
+					no = pstmt.executeUpdate();
+					log.fine("T_Acct_Balance Balance Adjusted update OK = " + no);
+				}
 				// Actualización del SALDO en base al debe y el haber
+				sqlUpdateBalance = new StringBuffer();
+				sqlUpdateBalance.append(" UPDATE ").append(getReportTableName());
+				sqlUpdateBalance.append(" SET Balance = Debit - Credit, ");
+				sqlUpdateBalance.append(" BalanceAdjusted = (CASE WHEN BalanceAdjusted IS NULL OR BalanceAdjusted = 0 THEN Debit - Credit ELSE BalanceAdjusted END) ");
+				if (p_C_ElementValue_To_ID != null) {
+					sqlUpdateBalance.append(", ");
+					sqlUpdateBalance.append(" c_elementvalue_to_id = " + p_C_ElementValue_To_ID );
+				}
+				sqlUpdateBalance.append(" WHERE AD_PInstance_ID = ? ");
 				pstmt = DB.prepareStatement(sqlUpdateBalance.toString(), get_TrxName(), true);
 				i = 1;
 				pstmt.setInt(i++, getAD_PInstance_ID());
 				no = pstmt.executeUpdate();
 				log.fine("T_Acct_Balance Balance update OK = " + no);
+				
+				// Ajustar el saldo del totalizado, sería el índice 0
+				sqlUpdateBalance = new StringBuffer();
+				sqlUpdateBalance.append(" UPDATE ").append(getReportTableName());
+				sqlUpdateBalance.append(" SET BalanceAdjusted = (select sum(BalanceAdjusted - Balance) FROM ").append(getReportTableName()).append(" WHERE AD_PInstance_ID = ? and subindex <> 0) ");
+				sqlUpdateBalance.append(" WHERE AD_PInstance_ID = ? and subindex = 0 ");
+				pstmt = DB.prepareStatement(sqlUpdateBalance.toString(), get_TrxName(), true);
+				i = 1;
+				pstmt.setInt(i++, getAD_PInstance_ID());
+				pstmt.setInt(i++, getAD_PInstance_ID());
+				no = pstmt.executeUpdate();
+				log.fine("T_Acct_Balance Balance Adjusted subindex 0 update OK = " + no);
 			}
 			
 			if(p_C_ElementValue_To_ID != null)
@@ -250,37 +271,110 @@ public class AccountsGeneralBalance extends AccountsHierarchicalReport {
 	}
 	
 	/**
-	 * @return Índice de inflación entre las fechas parámetro
+	 * Obtiene la view de la suma de la columna parámetro para realizar el update
+	 * correspondiente
+	 * 
+	 * @param columnSum          columna del select a sumar
+	 * @param withInflationIndex true si se debe incluir las porciones de índice de
+	 *                           inflación, false caso contrario
+	 * @return Consulta sql de suma de columna para un posterior update
 	 */
-	protected BigDecimal getInflationIndex() throws Exception{
-		BigDecimal inflationIndex = BigDecimal.ZERO;
+	protected String getSQLView(String columnSum, boolean withInflationIndex) {
+		StringBuffer sqlView = new StringBuffer();
+		sqlView.append(" SELECT ev.C_ElementValue_ID, tb.C_ElementValue_To_ID, tb.HierarchicalCode, COALESCE(SUM("+columnSum+"),0) as thesum "); 
+		sqlView.append(" FROM C_ElementValue ev ");
+		sqlView.append(" LEFT JOIN "+p_factAcctTable+" fa ON (fa.Account_ID = ev.C_ElementValue_ID) ");
+		sqlView.append(" INNER JOIN " + getReportTableName() + " tb ON (tb.C_ElementValue_ID = ev.C_ElementValue_ID AND tb.AD_PInstance_ID = ?) ");
+		if(withInflationIndex){
+			sqlView.append(" INNER JOIN c_period p on fa.DateAcct::date between startdate and enddate "); 
+			sqlView.append(" INNER JOIN c_inflation_index ii on p.c_period_id = ii.c_period_id "); 
+		}
+		sqlView.append(" WHERE ev.IsActive = 'Y' ");
+		sqlView.append("   AND fa.AD_Client_ID = ").append(getAD_Client_ID());
+		sqlAppend     ("   AND fa.AD_Org_ID = ? ", p_AD_Org_ID > 0, sqlView);
+		sqlAppend     ("   AND ?::date <= fa.DateAcct::date ", p_DateAcct_From, sqlView);
+		sqlAppend     ("   AND fa.DateAcct::date <= ?::date ", p_DateAcct_To, sqlView);
+		if(withInflationIndex) {
+			sqlView.append("   AND ev.isadjustable = 'Y' ");
+		}
+		sqlView.append(" GROUP BY ev.C_ElementValue_ID, tb.C_ElementValue_To_ID, tb.HierarchicalCode ");
+		return sqlView.toString();
+	}
+	
+	/**
+	 * Realiza validaciones de índices de inflación
+	 * @throws Exception
+	 */
+	protected void validationInflationIndex() throws Exception{
 		if(applyInflationIndexes){
+			// Fecha de inicio debe ser obligatoria
 			if(p_DateAcct_From == null){
 				throw new Exception(Msg.getMsg(getCtx(), "ApplyInflationIndexNotDateFrom"));
 			}
-			DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-			Timestamp dateTo = p_DateAcct_To != null ? p_DateAcct_To : Env.getDate();
+			
+			// Verificar que existan todos los períodos de inflación comprendidos entre la
+			// fecha de inicio y fin
+			// Si al menos 1 de los períodos no existe, entonces error
+			String sql = "select distinct c_period_id, name, startdate " + 
+					"from c_period p " + 
+					"where ad_client_id = ? " +
+					"	and startdate >= date_trunc('month', ?::date) ";
+			if(p_DateAcct_To != null) {
+				sql += "	and startdate <= date_trunc('month', ?::date) ";
+			}
+			sql += "except " + 
+					"select p.c_period_id, p.name, p.startdate " + 
+					"from c_inflation_index ii " + 
+					"join c_period p on p.c_period_id = ii.c_period_id " + 
+					"where p.ad_client_id = ? " + 
+					"	and startdate >= date_trunc('month', ?::date) ";
+			if(p_DateAcct_To != null) {
+				sql += "	and startdate <= date_trunc('month', ?::date)";
+			}
+			sql += " ORDER BY startdate ";
+					
+			PreparedStatement ps = DB.prepareStatement(sql, get_TrxName(), true);
+			int i = 1;
+			ps.setInt(i++, getAD_Client_ID());
+			ps.setTimestamp(i++, p_DateAcct_From);
+			if(p_DateAcct_To != null) {
+				ps.setTimestamp(i++, p_DateAcct_To);
+			}
+			ps.setInt(i++, getAD_Client_ID());
+			ps.setTimestamp(i++, p_DateAcct_From);
+			if(p_DateAcct_To != null) {
+				ps.setTimestamp(i++, p_DateAcct_To);
+			}
+			ResultSet rs = ps.executeQuery();
+			String periodNames = "";
+			while(rs.next()) {
+				periodNames += ","+rs.getString("name");
+			}
+			rs.close();
+			ps.close();
+			if(periodNames.length() > 0) {
+				periodNames = periodNames.substring(1);
+				throw new Exception(Msg.getMsg(getCtx(), "Los siguientes periodos no poseen índice de inflación cargado: "+periodNames));
+			}
+		}
+	}
+	
+	/**
+	 * @return Índice de inflación entre las fechas parámetro
+	 */
+	protected MInflationIndex getInflationIndex() throws Exception{
+		MInflationIndex fiFrom = null;
+		if(applyInflationIndexes){
+			// Validaciones de índices de inflación 
+			validationInflationIndex();
+			
 			// Obtener el indice de inflación entre fecha desde y hasta
 			int inflationIndexFromID = getInflationIndexID(p_DateAcct_From);
-			int inflationIndexToID = getInflationIndexID(dateTo);
-			MInflationIndex fiFrom = null, fiTo = null;
 			if(inflationIndexFromID > 0){
 				fiFrom = new MInflationIndex(getCtx(), inflationIndexFromID, get_TrxName());
 			}
-			else{
-				throw new Exception(Msg.getMsg(getCtx(), "ApplyInflationIndexNotPeriod",
-						new Object[] { df.format(p_DateAcct_From) }));
-			}
-			if(inflationIndexToID > 0){
-				fiTo = new MInflationIndex(getCtx(), inflationIndexToID, get_TrxName());
-			}
-			else{
-				throw new Exception(
-						Msg.getMsg(getCtx(), "ApplyInflationIndexNotPeriod", new Object[] { df.format(dateTo) }));
-			}
-			inflationIndex = fiFrom.getInflationIndex().compareTo(BigDecimal.ZERO) == 0 ? BigDecimal.ZERO : (fiTo.getInflationIndex().subtract(fiFrom.getInflationIndex()).divide(fiFrom.getInflationIndex(), 4, BigDecimal.ROUND_HALF_UP));
 		}
-		return inflationIndex;
+		return fiFrom;
 	}
 	
 	/**
