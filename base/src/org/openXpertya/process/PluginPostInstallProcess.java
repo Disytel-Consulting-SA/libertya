ce96703e8dfe6df0abab7d8ec9ffc7166e46b8b6
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2021-02-01 16:57:20 +0000

    Refactor y encapsulamiento de actualización de informes en postinstall

--- base/src/org/openXpertya/process/PluginPostInstallProcess.java
+++ base/src/org/openXpertya/process/PluginPostInstallProcess.java
@@ -3,8 +3,13 @@ package org.openXpertya.process;
 import java.io.File;
 import java.util.HashMap;
 
+import org.openXpertya.JasperReport.MJasperReport;
+import org.openXpertya.model.MProcess;
+import org.openXpertya.model.X_AD_JasperReport;
+import org.openXpertya.model.X_AD_Process;
 import org.openXpertya.plugin.common.PluginConstants;
 import org.openXpertya.plugin.install.PluginXMLUpdaterPostInstall;
+import org.openXpertya.utils.JarHelper;
 
 public class PluginPostInstallProcess extends SvrProcess {
 
@@ -59,4 +64,45 @@ public class PluginPostInstallProcess extends SvrProcess {
 		return PluginConstants.POSTINSTALL_BINARIES_DIR + "/" + fileName;
 	}
 
+	/**
+	 * Actualiza el jasper report parámetro
+	 * 
+	 * @param uid      UID del Jasper Report
+	 * @param filename Nombre del archivo
+	 */
+	protected void updateJasperReport(String uid, String filename) throws Exception {
+		MJasperReport.updateBinaryData(get_TrxName(), getCtx(), uid,
+				JarHelper.readBinaryFromJar(jarFileURL, getBinaryFileURL(filename)));
+	}
+	
+	/**
+	 * Actualiza el adjunto del proceso parámetro
+	 * 
+	 * @param uid      UID del AD_Process
+	 * @param filename Nombre del archivo
+	 */
+	protected void updateAttachment(String uid, String filename) throws Exception {
+		MProcess.addAttachment(get_TrxName(), getCtx(), uid, filename,
+				JarHelper.readBinaryFromJar(jarFileURL, getBinaryFileURL(filename)));
+	}
+	
+	/**
+	 * Actualiza un informe, si el UID contiene AD_Process es un attachment y si
+	 * contiene AD_JasperReport es un Jasper Report. Extender el método en caso de
+	 * ser necesario actualizar otros componentes a futuro
+	 * 
+	 * @param uid      UID del componente de LY
+	 * @param filename Nombre del Archivo
+	 * @throws Exception
+	 */
+	protected void updateReport(String uid, String filename) throws Exception {
+		if(filename != null && uid != null) {
+			if(uid.contains(X_AD_Process.Table_Name)) {
+				updateAttachment(uid, filename);
+			}
+			else if(uid.contains(X_AD_JasperReport.Table_Name)) {
+				updateJasperReport(uid, filename);
+			}
+		}
+	}
 }
