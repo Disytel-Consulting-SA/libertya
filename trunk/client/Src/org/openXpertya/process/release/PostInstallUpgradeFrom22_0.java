44ebbb5208b17632bcba4211b1dfd5ba7299ea89
fcristina <fcristina@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2022-12-15 13:55:29 +0000

    Estructura de soporte para PostInstallFrom22

---
+++ trunk/client/Src/org/openXpertya/process/release/PostInstallUpgradeFrom22_0.java
@@ -0,0 +1,16 @@
+package org.openXpertya.process.release;
+
+import org.openXpertya.process.PluginPostInstallProcess;
+
+public class PostInstallUpgradeFrom22_0 extends PluginPostInstallProcess {
+	
+	@Override
+	protected String doIt() throws Exception {
+		super.doIt();
+		
+		// Incorporación de informes
+		
+		return "";
+	}
+	
+}
