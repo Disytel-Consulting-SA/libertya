6127d567fe0a53077cc4d0896763362fcf369f8e
fcristina <fcristina@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2023-03-17 14:07:07 +0000

    Relacionado con commit anterior. Adecuacion preinstall segun orden cronologico de merge.

--- trunk/data/core/upgrade_from_22.0/preinstall_from_22.0.sql
+++ trunk/data/core/upgrade_from_22.0/preinstall_from_22.0.sql
@@ -6,7 +6,165 @@
 -- 	2) Recordar realizar las adiciones con un comentario con formato YYYYMMDD-HHMM
 -- ========================================================================================
 
---20220418-1000 Se agregan a las tablas C_Order, C_Invoice y M_InOut los campos necesarios para persistir los datos principales de la EC asociada, para evitar problemas al la hora de imprimir el documento cuando cuando se modifican datos de la EC.
+--20220915-1036 Nuevos indices para informe de balance
+CREATE INDEX fact_acct_dateacct_date ON fact_acct ((dateacct::date));
+update ad_system set dummy = (SELECT addindexifnotexists('c_elementvalue_active', 'c_elementvalue', 'isactive', 'where isactive = ''Y'''));
+update ad_system set dummy = (SELECT addindexifnotexists('t_acct_balance_ev', 't_acct_balance', 'c_elementvalue_id'));
+update ad_system set dummy = (SELECT addindexifnotexists('t_acct_balance_pi', 't_acct_balance', 'ad_pinstance_id'));
+update ad_system set dummy = (SELECT addindexifnotexists('t_acct_balance_hc', 't_acct_balance', 'HierarchicalCode'));
+
+--20221111-1100 Compatibilidad con Postgres 12+.  
+--				La version anterior de addindexifnotexists se basaba en columnas oid, las cuales no son soportaedas a partir de Postgres 12+.
+--				De todas maneras las versiones recientes de postgres ya soportan el modificador IF NOT EXISTS,
+--				con lo cual el uso de esta function en los preinstalls ya no sería necesario.
+CREATE OR REPLACE FUNCTION addindexifnotexists(
+    indexname character varying,
+    tablename character varying,
+    columnname character varying,
+    whereclause character varying)
+  RETURNS numeric AS
+$BODY$
+DECLARE
+	existe integer;
+	psqlversion integer;
+BEGIN
+
+	-- Validar version de postgres (9.4 o inferiores no soportan IF NOT EXIST en la creacion de indices)
+	select into psqlversion substring((select version()), 12, (select position('.' in replace(lower((select version())), 'postgresql ', '')))-1)::integer;
+	if (psqlversion < 10) then
+
+		-- Creacion de indice mediante validacion de existencia previa 
+		select into existe count(1)
+		from
+		    pg_class t,
+		    pg_class i,
+		    pg_index ix,
+		    pg_attribute a
+		where
+		    t.oid = ix.indrelid
+		    and i.oid = ix.indexrelid
+		    and a.attrelid = t.oid
+		    and a.attnum = ANY(ix.indkey)
+		    and t.relkind = 'r'
+		    and lower(i.relname) = lower(indexname)
+		    and lower(t.relname) = lower(tablename)
+		    and lower(a.attname) = lower(columnname);
+	
+		IF (existe = 0) THEN
+			EXECUTE 'CREATE INDEX ' || indexname || 
+				' ON ' || tablename || 
+				' (' || columnname || ') ' || 
+				whereclause;
+			RETURN 1;
+		END IF;
+	
+		RETURN 0;
+	
+	else
+		-- Creacion de indice mediante nueva sintaxis soportada IF NOT EXISTS
+		EXECUTE 'CREATE INDEX IF NOT EXISTS ' || indexname || ' ON ' || tablename || ' (' || columnname || ') ' || whereclause;
+	end if;
+	RETURN 1;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION addindexifnotexists(character varying, character varying, character varying, character varying)
+  OWNER TO libertya;
+   
+--20230223-1158 Desactivacion de registros pendientes de replicacion con un age superior al umbral especificado
+/** 
+ * Desactiva registros si ya tienen cierto age considerable (a definir por el usuario)
+ * 
+ * Forma de uso:
+ * 	select * from replication_disable_old_records(1010016, '1 month'); 
+ * 
+ * Desactivara todos los registros de compañia 0 y la definida por parametro
+ * cuyo campo updated tenga una antiguedad mayor a un mes
+ */
+CREATE OR REPLACE FUNCTION replication_disable_old_records(p_clientid integer, p_age character varying)
+  RETURNS int AS
+$BODY$
+DECLARE
+	atable varchar;
+	query varchar;
+BEGIN
+	FOR atable IN (select table_name from information_schema.columns where lower(column_name) = 'reparray' and table_schema = 'libertya' order by table_name)  loop
+		begin
+			query = 'UPDATE ' || atable || ' SET reparray = ''SET''||reparray, includeinreplication = ''D''  WHERE ad_client_id IN (0, ' || p_clientid || ') AND includeinreplication = ''Y'' and age(now(), updated) > ''' || p_age || '''';
+			--raise notice '%', query;
+			execute query;
+		exception when others then
+			-- probablemente el campo updated no existe en la tabla
+			-- raise notice 'Error en tabla %', atable;
+		end;
+	END LOOP;
+	return 0;
+END
+$BODY$
+  LANGUAGE 'plpgsql' volatile;
+  
+  
+--20230228-1016 Desactivacion de registros pendientes de replicacion con un age superior al umbral especificado. Version mejorada que incluye numero de registros modificados
+/** 
+ * Desactiva registros si ya tienen cierto age considerable (a definir por el usuario)
+ * 
+ * Forma de uso:
+ * 	select * from replication_disable_old_records(1010016, '1 month'); 
+ * 
+ * Desactivara todos los registros de compañia 0 y la definida por parametro
+ * cuyo campo updated tenga una antiguedad mayor a un mes
+ */
+CREATE OR REPLACE FUNCTION replication_disable_old_records(p_clientid integer, p_age character varying)
+  RETURNS int AS
+$BODY$
+DECLARE
+	atable varchar;
+	query varchar;
+	whereclause VARCHAR;
+	cant int;
+	totalrecords int;
+begin
+	totalrecords = 0;
+	whereclause = ' WHERE ad_client_id IN (0, ' || p_clientid || ') AND includeinreplication = ''Y'' and age(now(), updated) > ''' || p_age || ''''; 
+	
+	FOR atable IN (    
+		-- iterar por todas las tablas configuradas en replicacion
+		SELECT lower(t.tablename)
+		FROM ad_tablereplication tr
+		INNER JOIN ad_table t ON tr.ad_table_id = t.ad_table_id
+		UNION
+		SELECT 'ad_changelog_replication'
+	)
+	loop
+		-- si no hay registros que cumplen la condicion, omitir
+		EXECUTE 'select count(1) from ' || atable || whereclause INTO cant;
+		if cant <= 0 THEN
+			continue;
+		END IF;
+		
+		-- notificar el numero de registros a bajar
+		totalrecords = totalrecords + cant;
+		raise notice '% : %', atable, cant;
+		query =    ' UPDATE ' || atable || ' SET reparray = ';
+		if atable <> 'ad_changelog_replication' then
+		    query = query || '''SET''||';
+		end if;
+		begin
+			query = query || 'reparray, includeinreplication = ''D'' ' || whereclause;  
+			--raise notice '%', query;
+			execute query;
+		exception when others then
+			-- probablemente el campo updated no existe en la tabla
+			-- raise notice 'Error en tabla %', atable;
+		end;
+	END LOOP;
+	return totalrecords;
+END
+$BODY$
+  LANGUAGE 'plpgsql' volatile;
+
+-- 20230316-1600 MERGE: 20220418-1000 Se agregan a las tablas C_Order, C_Invoice y M_InOut los campos necesarios para persistir los datos principales de la EC asociada, para evitar problemas al la hora de imprimir el documento cuando cuando se modifican datos de la EC.
 
 update ad_system set dummy = (SELECT addcolumnifnotexists('C_Order','Direccion','CHARACTER VARYING(40)'));
 update ad_system set dummy = (SELECT addcolumnifnotexists('C_Order','Localidad','CHARACTER VARYING(40)'));
@@ -28,20 +186,20 @@ update ad_system set dummy = (SELECT addcolumnifnotexists('M_InOut','Provincia',
 update ad_system set dummy = (SELECT addcolumnifnotexists('M_InOut','CP','CHARACTER VARYING(8)'));
 update ad_system set dummy = (SELECT addcolumnifnotexists('M_InOut','Cat_IVA_ID','INTEGER'));
 
--- 20220510-0930 Fix para que se muestre correctamente la suma de alícuotas en Libro IVA Compras CPBT.
+-- 20230316-1600 MERGE: 20220510-0930 Fix para que se muestre correctamente la suma de alícuotas en Libro IVA Compras CPBT.
 UPDATE e_electronicinvoiceref
 SET codigo = 'C'
 WHERE tabla_ref = 'TCOM' AND clave_busqueda = 'TIPO_CREDITO_FISCAL_COMPUTABLE';
 
--- 20220601 Se agrega en la tabla C_Tax el campo isNoGravado para indicar si el impuesto es No Gravado.
+-- 20230316-1600 MERGE: 20220601 Se agrega en la tabla C_Tax el campo isNoGravado para indicar si el impuesto es No Gravado.
 UPDATE ad_system SET dummy = (SELECT addcolumnifnotexists('C_tax','isNoGravado','character(1) NOT NULL DEFAULT ''N''::bpchar'));
 
--- 20220715 Fix para que Otros Impuestos BsAs sea categorizado como Otros Impuestos y aparezca en el campo 'Otros Tributos'
+-- 20230316-1600 MERGE: 20220715 Fix para que Otros Impuestos BsAs sea categorizado como Otros Impuestos y aparezca en el campo 'Otros Tributos'
 UPDATE C_Tax
 SET taxareatype = 'P'
 WHERE name = 'Otros impuestos BsAs';
 
--- 20220726 Rework para las funciones involucradas en las vistas reginfo para la exportación de libros IVA
+-- 20230316-1600 MERGE: 20220726 Rework para las funciones involucradas en las vistas reginfo para la exportación de libros IVA
 CREATE OR REPLACE FUNCTION libertya.getimportenogravado(p_c_invoice_id integer)
   RETURNS numeric AS
 $BODY$ 
@@ -252,7 +410,7 @@ END; $BODY$
 ALTER FUNCTION libertya.getcreditofiscalcomputable(integer)
   OWNER TO libertya;
 
--- 20220726 Rework de las vistas reginfo para la exportación de libros IVA
+-- 20230316-1600 MERGE: 20220726 Rework de las vistas reginfo para la exportación de libros IVA
 
 DROP VIEW IF EXISTS libertya.reginfo_compras_cbte_v;
 DROP VIEW IF EXISTS libertya.reginfo_compras_cbte_importacion_v;
@@ -680,161 +838,3 @@ WHERE
 	AND dt.isfiscaldocument = 'Y'::bpchar
 	AND (dt.isfiscal IS NULL OR dt.isfiscal = 'N'::bpchar OR dt.isfiscal = 'Y'::bpchar AND i.fiscalalreadyprinted = 'Y'::bpchar)
 ;
-
---20220915-1036 Nuevos indices para informe de balance
-CREATE INDEX fact_acct_dateacct_date ON fact_acct ((dateacct::date));
-update ad_system set dummy = (SELECT addindexifnotexists('c_elementvalue_active', 'c_elementvalue', 'isactive', 'where isactive = ''Y'''));
-update ad_system set dummy = (SELECT addindexifnotexists('t_acct_balance_ev', 't_acct_balance', 'c_elementvalue_id'));
-update ad_system set dummy = (SELECT addindexifnotexists('t_acct_balance_pi', 't_acct_balance', 'ad_pinstance_id'));
-update ad_system set dummy = (SELECT addindexifnotexists('t_acct_balance_hc', 't_acct_balance', 'HierarchicalCode'));
-
---20221111-1100 Compatibilidad con Postgres 12+.  
---				La version anterior de addindexifnotexists se basaba en columnas oid, las cuales no son soportaedas a partir de Postgres 12+.
---				De todas maneras las versiones recientes de postgres ya soportan el modificador IF NOT EXISTS,
---				con lo cual el uso de esta function en los preinstalls ya no sería necesario.
-CREATE OR REPLACE FUNCTION addindexifnotexists(
-    indexname character varying,
-    tablename character varying,
-    columnname character varying,
-    whereclause character varying)
-  RETURNS numeric AS
-$BODY$
-DECLARE
-	existe integer;
-	psqlversion integer;
-BEGIN
-
-	-- Validar version de postgres (9.4 o inferiores no soportan IF NOT EXIST en la creacion de indices)
-	select into psqlversion substring((select version()), 12, (select position('.' in replace(lower((select version())), 'postgresql ', '')))-1)::integer;
-	if (psqlversion < 10) then
-
-		-- Creacion de indice mediante validacion de existencia previa 
-		select into existe count(1)
-		from
-		    pg_class t,
-		    pg_class i,
-		    pg_index ix,
-		    pg_attribute a
-		where
-		    t.oid = ix.indrelid
-		    and i.oid = ix.indexrelid
-		    and a.attrelid = t.oid
-		    and a.attnum = ANY(ix.indkey)
-		    and t.relkind = 'r'
-		    and lower(i.relname) = lower(indexname)
-		    and lower(t.relname) = lower(tablename)
-		    and lower(a.attname) = lower(columnname);
-	
-		IF (existe = 0) THEN
-			EXECUTE 'CREATE INDEX ' || indexname || 
-				' ON ' || tablename || 
-				' (' || columnname || ') ' || 
-				whereclause;
-			RETURN 1;
-		END IF;
-	
-		RETURN 0;
-	
-	else
-		-- Creacion de indice mediante nueva sintaxis soportada IF NOT EXISTS
-		EXECUTE 'CREATE INDEX IF NOT EXISTS ' || indexname || ' ON ' || tablename || ' (' || columnname || ') ' || whereclause;
-	end if;
-	RETURN 1;
-END;
-$BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100;
-ALTER FUNCTION addindexifnotexists(character varying, character varying, character varying, character varying)
-  OWNER TO libertya;
-   
---20230223-1158 Desactivacion de registros pendientes de replicacion con un age superior al umbral especificado
-/** 
- * Desactiva registros si ya tienen cierto age considerable (a definir por el usuario)
- * 
- * Forma de uso:
- * 	select * from replication_disable_old_records(1010016, '1 month'); 
- * 
- * Desactivara todos los registros de compañia 0 y la definida por parametro
- * cuyo campo updated tenga una antiguedad mayor a un mes
- */
-CREATE OR REPLACE FUNCTION replication_disable_old_records(p_clientid integer, p_age character varying)
-  RETURNS int AS
-$BODY$
-DECLARE
-	atable varchar;
-	query varchar;
-BEGIN
-	FOR atable IN (select table_name from information_schema.columns where lower(column_name) = 'reparray' and table_schema = 'libertya' order by table_name)  loop
-		begin
-			query = 'UPDATE ' || atable || ' SET reparray = ''SET''||reparray, includeinreplication = ''D''  WHERE ad_client_id IN (0, ' || p_clientid || ') AND includeinreplication = ''Y'' and age(now(), updated) > ''' || p_age || '''';
-			--raise notice '%', query;
-			execute query;
-		exception when others then
-			-- probablemente el campo updated no existe en la tabla
-			-- raise notice 'Error en tabla %', atable;
-		end;
-	END LOOP;
-	return 0;
-END
-$BODY$
-  LANGUAGE 'plpgsql' volatile;
-  
-  
---20230228-1016 Desactivacion de registros pendientes de replicacion con un age superior al umbral especificado. Version mejorada que incluye numero de registros modificados
-/** 
- * Desactiva registros si ya tienen cierto age considerable (a definir por el usuario)
- * 
- * Forma de uso:
- * 	select * from replication_disable_old_records(1010016, '1 month'); 
- * 
- * Desactivara todos los registros de compañia 0 y la definida por parametro
- * cuyo campo updated tenga una antiguedad mayor a un mes
- */
-CREATE OR REPLACE FUNCTION replication_disable_old_records(p_clientid integer, p_age character varying)
-  RETURNS int AS
-$BODY$
-DECLARE
-	atable varchar;
-	query varchar;
-	whereclause VARCHAR;
-	cant int;
-	totalrecords int;
-begin
-	totalrecords = 0;
-	whereclause = ' WHERE ad_client_id IN (0, ' || p_clientid || ') AND includeinreplication = ''Y'' and age(now(), updated) > ''' || p_age || ''''; 
-	
-	FOR atable IN (    
-		-- iterar por todas las tablas configuradas en replicacion
-		SELECT lower(t.tablename)
-		FROM ad_tablereplication tr
-		INNER JOIN ad_table t ON tr.ad_table_id = t.ad_table_id
-		UNION
-		SELECT 'ad_changelog_replication'
-	)
-	loop
-		-- si no hay registros que cumplen la condicion, omitir
-		EXECUTE 'select count(1) from ' || atable || whereclause INTO cant;
-		if cant <= 0 THEN
-			continue;
-		END IF;
-		
-		-- notificar el numero de registros a bajar
-		totalrecords = totalrecords + cant;
-		raise notice '% : %', atable, cant;
-		query =    ' UPDATE ' || atable || ' SET reparray = ';
-		if atable <> 'ad_changelog_replication' then
-		    query = query || '''SET''||';
-		end if;
-		begin
-			query = query || 'reparray, includeinreplication = ''D'' ' || whereclause;  
-			--raise notice '%', query;
-			execute query;
-		exception when others then
-			-- probablemente el campo updated no existe en la tabla
-			-- raise notice 'Error en tabla %', atable;
-		end;
-	END LOOP;
-	return totalrecords;
-END
-$BODY$
-  LANGUAGE 'plpgsql' volatile;
