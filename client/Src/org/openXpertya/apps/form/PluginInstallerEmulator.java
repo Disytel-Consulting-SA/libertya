fab6e8e34298f75867644f6a79b9d1aa46f625fa
Federico Cristina <federico.cristina@disytel.net>
2024-11-25 11:09:23 -0300

    Emulacion de instalacion de componente.  Simplificar argumentos de invocaciones

--- client/Src/org/openXpertya/apps/form/PluginInstallerEmulator.java
+++ client/Src/org/openXpertya/apps/form/PluginInstallerEmulator.java
@@ -5,8 +5,12 @@ import org.openXpertya.util.Env;
 public class PluginInstallerEmulator extends PluginInstaller {
 	
 	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.err.println("Debe especificar la ruta al archivo de instalacion");
+			System.exit(1);
+		}
 		Env.setContext(Env.getCtx(), "#EmulateInstall", "Y");
-		PluginInstaller.main(args);
+		PluginInstaller.main(new String[] {args[0], "N", "N"});
 	}
 
 }
