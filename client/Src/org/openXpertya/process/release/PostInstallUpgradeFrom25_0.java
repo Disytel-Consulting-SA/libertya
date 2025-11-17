9f821455c66d41777f878346654288d26fbdc1fc
fcristina77 <federico.cristina@disytel.net>
2025-11-17 09:33:08 -0300

    Estructura para nueva version / componentversion

---
+++ client/Src/org/openXpertya/process/release/PostInstallUpgradeFrom25_0.java
@@ -0,0 +1,17 @@
+package org.openXpertya.process.release;
+
+import org.openXpertya.JasperReport.MJasperReport;
+import org.openXpertya.model.MProcess;
+import org.openXpertya.process.PluginPostInstallProcess;
+import org.openXpertya.utils.JarHelper;
+
+public class PostInstallUpgradeFrom25_0 extends PluginPostInstallProcess {
+	
+	@Override
+	protected String doIt() throws Exception {
+		super.doIt();
+		
+		return "";
+	}
+	
+}
