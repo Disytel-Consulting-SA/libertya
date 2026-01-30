fee8d779e8cb45568e184110fb264a484d5b718f
jviejo18 <julian.viejo@disytel.net>
2026-01-30 14:01:49 -0300

    Fix InventoryType

--- data/core/upgrade_from_25.0/preinstall_from_25.0.sql
+++ data/core/upgrade_from_25.0/preinstall_from_25.0.sql
@@ -1,3 +1,8 @@
 -- 20260123 Fix correspondiente al micro de facturacion (metadata faltante)
 update libertya.ad_system set dummy = (SELECT libertya.addcolumnifnotexists('C_POSJournal','iscontingencia','character(1)'));
 
+-- Fix A-400 Inventario fisico y anulación – Comportamiento incorrecto del inventario físico al utilizar el tipo “Sobrescribir”
+ALTER TABLE IF EXISTS libertya.m_inventory ADD inventorytype bpchar(1) DEFAULT 'D'::bpchar NOT NULL;
+ALTER TABLE IF EXISTS libertya.m_inventory ALTER COLUMN inventorytype SET STORAGE EXTENDED;
+
+
