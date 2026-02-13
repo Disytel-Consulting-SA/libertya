8da4e30b81809d619f3f249c44183c3ad7f680a9
jviejo18 <julian.viejo@disytel.net>
2026-02-13 12:36:01 -0300

    Function getfechaconversion

--- data/core/upgrade_from_25.0/preinstall_from_25.0.sql
+++ data/core/upgrade_from_25.0/preinstall_from_25.0.sql
@@ -6,3 +6,23 @@ ALTER TABLE IF EXISTS libertya.m_inventory ADD inventorytype bpchar(1) DEFAULT '
 ALTER TABLE IF EXISTS libertya.m_inventory ALTER COLUMN inventorytype SET STORAGE EXTENDED;
 
 
+-- 2026-02-13
+create or replace
+function libertya.getfechaconversion(p_timestamp timestamp without time zone,
+p_cancelamismamoneda character)
+ returns date
+ language plpgsql
+as $function$ declare fecha_base DATE := (p_timestamp - interval '1 day')::DATE;
+dia_semana INT;
+begin if coalesce(p_CancelaMismaMoneda, 'N') = 'N' then return p_timestamp;
+end if;
+dia_semana := extract(DOW from fecha_base);
+if dia_semana = 0 then return fecha_base - interval '2 day';
+elsif dia_semana = 6 then return fecha_base - interval '1 day';
+else return fecha_base;
+end if;
+end;
+$function$
+;
+
+
