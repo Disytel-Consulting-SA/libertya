11ffb4e81c3feac9ac814f482c56ca39b2a47744
jviejo18 <julian.viejo@disytel.net>
2026-02-13 13:46:24 -0300

    Merge metadata import_facturas_proveedor_afip version 3.0

--- data/core/upgrade_from_25.0/preinstall_from_25.0.sql
+++ data/core/upgrade_from_25.0/preinstall_from_25.0.sql
@@ -25,4 +25,24 @@ end;
 $function$
 ;
 
+-- merge org.libertya.core.micro.r2993.dev.import_facturas_proveedor_afip v3.0 (upgrade_from_2.0)
+update ad_system set dummy = (SELECT addcolumnifnotexists('i_vendor_invoice_import','tipoidentificacionrecep','varchar(2)'));
+update ad_system set dummy = (SELECT addcolumnifnotexists('i_vendor_invoice_import','numeroidentificacionrecep','varchar(20)'));
+update ad_system set dummy = (SELECT addcolumnifnotexists('i_vendor_invoice_import','netogravado_0','numeric(20, 2)'));
+update ad_system set dummy = (SELECT addcolumnifnotexists('i_vendor_invoice_import','netogravado_2_5','numeric(20, 2)'));
+update ad_system set dummy = (SELECT addcolumnifnotexists('i_vendor_invoice_import','iva_2_5','numeric(20, 2)'));
+update ad_system set dummy = (SELECT addcolumnifnotexists('i_vendor_invoice_import','netogravado_5','numeric(20, 2)'));
+update ad_system set dummy = (SELECT addcolumnifnotexists('i_vendor_invoice_import','iva_5','numeric(20, 2)'));
+update ad_system set dummy = (SELECT addcolumnifnotexists('i_vendor_invoice_import','netogravado_10_5','numeric(20, 2)'));
+update ad_system set dummy = (SELECT addcolumnifnotexists('i_vendor_invoice_import','iva_10_5','numeric(20, 2)'));
+update ad_system set dummy = (SELECT addcolumnifnotexists('i_vendor_invoice_import','netogravado_21','numeric(20, 2)'));
+update ad_system set dummy = (SELECT addcolumnifnotexists('i_vendor_invoice_import','iva_21','numeric(20, 2)'));
+update ad_system set dummy = (SELECT addcolumnifnotexists('i_vendor_invoice_import','netogravado_27','numeric(20, 2)'));
+update ad_system set dummy = (SELECT addcolumnifnotexists('i_vendor_invoice_import','iva_27','numeric(20, 2)'));
+update ad_system set dummy = (SELECT addcolumnifnotexists('i_vendor_invoice_import','netogravadototal','numeric(20, 2)'));
+update ad_system set dummy = (SELECT addcolumnifnotexists('i_vendor_invoice_import','otrostributos','numeric(20, 2)'));
 
+INSERT INTO libertya.ad_preference (ad_preference_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, ad_window_id, ad_user_id, "attribute", value)
+VALUES(nextval('seq_ad_preference'), 1010016, 0, 'Y', current_timestamp, 100, current_timestamp, 100, NULL, NULL, 'Default_OtrosTributosTax_Vendors', '01 Percepcion IVA');
+
+UPDATE libertya.ad_preference SET value='IVA 0%' WHERE attribute='Default_NoGravadoTax_Vendors';
