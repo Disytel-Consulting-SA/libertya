b02ab2a9e480b09563ef43e215056c5b44436444
jviejo18 <julian.viejo@disytel.net>
2026-08-04 18:03:41 -0300

    Fix Exception: relation ad_protected_access does not exist

--- base/src/org/openXpertya/model/MTab.java
+++ base/src/org/openXpertya/model/MTab.java
@@ -2443,7 +2443,7 @@ public class MTab implements DataStatusListener,Evaluatee,Serializable {
             return;
         }
 
-        String SQL = "SELECT Record_ID " + "FROM AD_protected_Access " + "WHERE AD_User_ID=? AND AD_Table_ID=? AND IsActive='Y' " + "ORDER BY Record_ID";
+        String SQL = "SELECT Record_ID " + "FROM " + MPrivateAccess.Table_Name + " " + "WHERE AD_User_ID=? AND AD_Table_ID=? AND IsActive='Y' " + "ORDER BY Record_ID";
 
         // dREHER asegurar cierre de conexiones
         PreparedStatement pstmt = null;
@@ -2469,19 +2469,11 @@ public class MTab implements DataStatusListener,Evaluatee,Serializable {
                 m_Lock.add( key );
             }
 
-            rs.close();
-            pstmt.close();
         } catch( SQLException e ) {
             log.log( Level.SEVERE,"loadLocks",e );
         } finally {
-        	try {
-				rs.close();
-				pstmt.close();
-				rs = null; pstmt = null;
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+            DB.close(rs, pstmt);
+            rs = null; pstmt = null;
         }
 
         log.fine( "loadLocks #" + m_Lock.size());
