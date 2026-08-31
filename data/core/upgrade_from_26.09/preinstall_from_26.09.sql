b6ee164533e94123ac094088ac48ec6e7f279d94
fcristina77 <federico.cristina@disytel.net>
2026-08-31 08:47:10 -0300

    Estructura inicial para nueva version

---
+++ data/core/upgrade_from_26.09/preinstall_from_26.09.sql
@@ -0,0 +1,9 @@
+-- ========================================================================================
+-- PREINSTALL FROM 26.09
+-- ========================================================================================
+-- Consideraciones importantes:
+--	1) NO hacer cambios en el archivo, realizar siempre APPENDs al final del mismo 
+-- 	2) Recordar realizar las adiciones con un comentario con formato YYYYMMDD-HHMM
+-- ========================================================================================
+
+-- 20260831-0846: Preinstall from 26.09
